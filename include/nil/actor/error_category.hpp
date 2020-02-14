d5ef36fcf68f296d9724cc775b6a67e19a0707ef
Mikhail Komarov <nemo@nil.foundation>
2020-02-14 15:46:52 +0300

    Missing files implemented.

---
+++ include/nil/actor/error_category.hpp
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2011-2019 Dominik Charousset
+// Copyright (c) 2018-2020 Nil Foundation AG
+// Copyright (c) 2018-2020 Mikhail Komarov <nemo@nil.foundation>
+//
+// Distributed under the terms and conditions of the BSD 3-Clause License or
+// (at your option) under the terms and conditions of the Boost Software
+// License 1.0. See accompanying files LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt.
+//---------------------------------------------------------------------------//
+
+#pragma once
+
+#include <cstdint>
+
+namespace nil {
+    namespace actor {
+
+        /// Customization point for enabling conversion from an enum type to an ::error.
+        template<class T>
+        struct error_category;
+    }    // namespace actor
+}    // namespace nil
