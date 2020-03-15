9a107f07ac5982641b81de4129dd43c143f845da
Mikhail Komarov <nemo@nil.foundation>
2020-03-15 13:15:37 +0300

    Minor copyright updates #1

--- include/nil/actor/detail/functor_attachable.hpp
+++ include/nil/actor/detail/functor_attachable.hpp
@@ -1,20 +1,14 @@
-/******************************************************************************
- *                       ____    _    _____                                   *
- *                      / ___|  / \  |  ___|    C++                           *
- *                     | |     / _ \ | |_       Actor                         *
- *                     | |___ / ___ \|  _|      Framework                     *
- *                      \____/_/   \_|_|                                      *
- *                                                                            *
- * Copyright 2011-2018 Dominik Charousset                                     *
- *                                                                            *
- * Distributed under the terms and conditions of the BSD 3-Clause License or  *
- * (at your option) under the terms and conditions of the Boost Software      *
- * License 1.0. See accompanying files LICENSE and LICENCE_ALTERNATIVE.       *
- *                                                                            *
- * If you did not receive a copy of the license files, see                    *
- * http://opensource.org/licenses/BSD-3-Clause and                            *
- * http://www.boost.org/LICENSE_1_0.txt.                                      *
- ******************************************************************************/
+//---------------------------------------------------------------------------//
+// Copyright (c) 2011-2018 Dominik Charousset
+// Copyright (c) 2018-2019 Nil Foundation AG
+// Copyright (c) 2018-2019 Mikhail Komarov <nemo@nil.foundation>
+//
+// Distributed under the terms and conditions of the BSD 3-Clause License or
+// (at your option) under the terms and conditions of the Boost Software
+// License 1.0. See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt for Boost License or
+// http://opensource.org/licenses/BSD-3-Clause for BSD 3-Clause License
+//---------------------------------------------------------------------------//
 
 #pragma once
 
