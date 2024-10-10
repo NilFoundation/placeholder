fa18be4ceccee432a5a6788a20de7765971aae7b
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-10-10 13:20:14 +0200

    add log output

--- parallel-crypto3/libs/parallel-zk/include/nil/crypto3/zk/snark/systems/plonk/placeholder/prover.hpp
+++ parallel-crypto3/libs/parallel-zk/include/nil/crypto3/zk/snark/systems/plonk/placeholder/prover.hpp
@@ -137,6 +137,7 @@ namespace nil {
 
                     placeholder_proof<FieldType, ParamsType> process() {
                         PROFILE_SCOPE("Placeholder prover, total time");
+                        BOOST_LOG_TRIVIAL(info) << "running mutithreaded mode";
 
                         // 2. Commit witness columns and public_input columns
                         _commitment_scheme.append_to_batch(VARIABLE_VALUES_BATCH, _polynomial_table->witnesses());
