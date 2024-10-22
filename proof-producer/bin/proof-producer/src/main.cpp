09a739574ede0e464e59d175a9149f5cab25b36b
e.tatuzova <e.tatuzova@nil.foundation>
2024-10-22 11:43:36 +0400

    evm-verifier option added to proof-producer#100

--- proof-producer/bin/proof-producer/src/main.cpp
+++ proof-producer/bin/proof-producer/src/main.cpp
@@ -45,15 +45,17 @@ int run_prover(const nil::proof_generator::ProverOptions& prover_options) {
                     prover_result =
                         prover.read_circuit(prover_options.circuit_file_path) &&
                         prover.read_assignment_table(prover_options.assignment_table_file_path) &&
+                        prover.print_public_input_for_evm(prover_options.evm_verifier_path) &&
                         prover.preprocess_public_data() &&
                         prover.preprocess_private_data() &&
                         prover.generate_to_file(
                             prover_options.proof_file_path,
                             prover_options.json_file_path,
-                            false/*don't skip verification*/) && 
+                            false/*don't skip verification*/) &&
                         prover.save_preprocessed_common_data_to_file(prover_options.preprocessed_common_data_path) &&
                         prover.save_public_preprocessed_data_to_file(prover_options.preprocessed_public_data_path) &&
-                        prover.save_commitment_state_to_file(prover_options.commitment_scheme_state_path);
+                        prover.save_commitment_state_to_file(prover_options.commitment_scheme_state_path) &&
+                        prover.print_evm_verifier(prover_options.evm_verifier_path);
                     break;
                 case nil::proof_generator::detail::ProverStage::PREPROCESS:
                     prover_result =
@@ -63,20 +65,23 @@ int run_prover(const nil::proof_generator::ProverOptions& prover_options) {
                         prover.preprocess_public_data() &&
                         prover.save_preprocessed_common_data_to_file(prover_options.preprocessed_common_data_path) &&
                         prover.save_public_preprocessed_data_to_file(prover_options.preprocessed_public_data_path) &&
-                        prover.save_commitment_state_to_file(prover_options.commitment_scheme_state_path);
+                        prover.save_commitment_state_to_file(prover_options.commitment_scheme_state_path)&&
+                        prover.print_evm_verifier(prover_options.evm_verifier_path);
                     break;
                 case nil::proof_generator::detail::ProverStage::PROVE:
                     // Load preprocessed data from file and generate the proof.
                     prover_result =
                         prover.read_circuit(prover_options.circuit_file_path) &&
                         prover.read_assignment_table(prover_options.assignment_table_file_path) &&
+                        prover.print_public_input_for_evm(prover_options.evm_verifier_path) &&
                         prover.read_public_preprocessed_data_from_file(prover_options.preprocessed_public_data_path) &&
                         prover.read_commitment_scheme_from_file(prover_options.commitment_scheme_state_path) &&
                         prover.preprocess_private_data() &&
                         prover.generate_to_file(
                             prover_options.proof_file_path,
                             prover_options.json_file_path,
-                            true/*skip verification*/);
+                            true/*skip verification*/)&&
+                        prover.print_evm_verifier(prover_options.evm_verifier_path);
                     break;
                 case nil::proof_generator::detail::ProverStage::GENERATE_PARTIAL_PROOF:
                     // Load preprocessed data from file and generate the proof.
@@ -116,14 +121,14 @@ int run_prover(const nil::proof_generator::ProverOptions& prover_options) {
                             prover_options.proof_file_path);
                     break;
                 case nil::proof_generator::detail::ProverStage::COMPUTE_COMBINED_Q:
-                    prover_result = 
+                    prover_result =
                         prover.read_commitment_scheme_from_file(prover_options.commitment_scheme_state_path) &&
                         prover.generate_combined_Q_to_file(
                             prover_options.aggregated_challenge_file, prover_options.combined_Q_starting_power,
                             prover_options.combined_Q_polynomial_file);
                     break;
                 case nil::proof_generator::detail::ProverStage::GENERATE_AGGREGATED_FRI_PROOF:
-                    prover_result = 
+                    prover_result =
                         prover.read_assignment_description(prover_options.assignment_description_file_path) &&
                         prover.generate_aggregated_FRI_proof_to_file(
                             prover_options.aggregated_challenge_file,
@@ -133,7 +138,7 @@ int run_prover(const nil::proof_generator::ProverOptions& prover_options) {
                             prover_options.consistency_checks_challenges_file);
                     break;
                 case nil::proof_generator::detail::ProverStage::GENERATE_CONSISTENCY_CHECKS_PROOF:
-                    prover_result = 
+                    prover_result =
                         prover.read_commitment_scheme_from_file(prover_options.commitment_scheme_state_path) &&
                         prover.generate_consistency_checks_to_file(
                             prover_options.combined_Q_polynomial_file,
