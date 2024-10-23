5589a2ecd278a64b3316ff8647d7509f40479b72
ayashunsky <alexey.yashunsky@gmail.com>
2024-10-23 17:58:21 +0200

    Initial parameter processing

--- crypto3/libs/blueprint/include/nil/blueprint/bbf/generic.hpp
+++ crypto3/libs/blueprint/include/nil/blueprint/bbf/generic.hpp
@@ -661,11 +661,27 @@ namespace nil {
                              typename FieldType::value_type>::type;
                 using context_type = context<FieldType, stage>;
                 using plonk_copy_constraint = crypto3::zk::snark::plonk_copy_constraint<FieldType>;
+                struct table_params {
+                    std::size_t witnesses;
+                    std::size_t public_inputs;
+                    std::size_t constants;
+                    std::size_t rows;
+                };
+                public:
+                    using TYPE = typename std::conditional<static_cast<bool>(stage),
+                                 crypto3::zk::snark::plonk_constraint<FieldType>,
+                                 typename FieldType::value_type>::type;
+                    using context_type = context<FieldType, stage>;
+                    using plonk_copy_constraint = crypto3::zk::snark::plonk_copy_constraint<FieldType>;
 
             private:
                 context_type &ct;
 
             public:
+                    static table_params get_minimal_requirements() {
+                        return {0,0,0,0};
+                    }
+
                 void allocate(TYPE &C, column_type t = column_type::witness) {
                     auto [col, row] = ct.next_free_cell(t);
                     ct.allocate(C, col, row, t);
