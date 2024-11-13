993d68553509e9072d55866d0fb71981e5d4ab08
Andrey Nefedov <ioxid@nil.foundation>
2024-11-13 15:29:49 +0000

    Remove more boost

--- crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/big_integer/detail/config.hpp
+++ crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/big_integer/detail/config.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <bits/endian.h>
+
 #if !defined(NIL_CO3_MP_FORCEINLINE)
 
 #if defined(NDEBUG) && !defined(_DEBUG)
@@ -17,6 +19,35 @@
 
 #define NIL_CO3_MP_FORCEINLINE inline
 
+#endif
+
+#if defined(__BYTE_ORDER)
+#if defined(__BIG_ENDIAN) && (__BYTE_ORDER == __BIG_ENDIAN)
+#undef NIL_CO3_MP_ENDIAN_BIG_BYTE
+#define NIL_CO3_MP_ENDIAN_BIG_BYTE 1
+#endif
+#if defined(__LITTLE_ENDIAN) && (__BYTE_ORDER == __LITTLE_ENDIAN)
+#undef NIL_CO3_MP_ENDIAN_LITTLE_BYTE
+#define NIL_CO3_MP_ENDIAN_LITTLE_BYTE 1
+#endif
+#if defined(__PDP_ENDIAN) && (__BYTE_ORDER == __PDP_ENDIAN)
+#undef NIL_CO3_MP_ENDIAN_LITTLE_WORD
+#define NIL_CO3_MP_ENDIAN_LITTLE_WORD 1
+#endif
+#endif
+#if !defined(__BYTE_ORDER) && defined(_BYTE_ORDER)
+#if defined(_BIG_ENDIAN) && (_BYTE_ORDER == _BIG_ENDIAN)
+#undef NIL_CO3_MP_ENDIAN_BIG_BYTE
+#define NIL_CO3_MP_ENDIAN_BIG_BYTE 1
+#endif
+#if defined(_LITTLE_ENDIAN) && (_BYTE_ORDER == _LITTLE_ENDIAN)
+#undef NIL_CO3_MP_ENDIAN_LITTLE_BYTE
+#define NIL_CO3_MP_ENDIAN_LITTLE_BYTE 1
+#endif
+#if defined(_PDP_ENDIAN) && (_BYTE_ORDER == _PDP_ENDIAN)
+#undef NIL_CO3_MP_ENDIAN_LITTLE_WORD
+#define NIL_CO3_MP_ENDIAN_LITTLE_WORD 1
+#endif
 #endif
 
 #endif
