ca5b53378441dc9c662dea336130b5d9c2ae9ef3
ayashunsky <alexey.yashunsky@gmail.com>
2024-11-23 13:37:14 +0100

    Interfaces reorganized

--- crypto3/libs/blueprint/include/nil/blueprint/bbf/generic.hpp
+++ crypto3/libs/blueprint/include/nil/blueprint/bbf/generic.hpp
@@ -660,19 +660,13 @@ namespace nil {
 
             template<typename FieldType, GenerationStage stage>
             class generic_component {
-            public:
-                using TYPE = typename std::conditional<static_cast<bool>(stage),
-                             crypto3::zk::snark::plonk_constraint<FieldType>,
-                             typename FieldType::value_type>::type;
-                using context_type = context<FieldType, stage>;
-                using plonk_copy_constraint = crypto3::zk::snark::plonk_copy_constraint<FieldType>;
-                struct table_params {
-                    std::size_t witnesses;
-                    std::size_t public_inputs;
-                    std::size_t constants;
-                    std::size_t rows;
-                };
                 public:
+                    struct table_params {
+                        std::size_t witnesses;
+                        std::size_t public_inputs;
+                        std::size_t constants;
+                        std::size_t rows;
+                    };
                     using TYPE = typename std::conditional<static_cast<bool>(stage),
                                  crypto3::zk::snark::plonk_constraint<FieldType>,
                                  typename FieldType::value_type>::type;
