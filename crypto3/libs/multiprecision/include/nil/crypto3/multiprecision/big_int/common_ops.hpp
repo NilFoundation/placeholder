827c365b3e12ef8051d9bfd90dbc5a5b1bb64f02
Andrey Nefedov <ioxid@nil.foundation>
2024-11-26 12:31:53 +0000

    Refactor big_int for algebra 4

--- crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/big_int/common_ops.hpp
+++ crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/big_int/common_ops.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstddef>
+#include <type_traits>
+
 #include "nil/crypto3/multiprecision/big_int/big_uint.hpp"
 
 namespace nil::crypto3::multiprecision {
@@ -22,7 +25,7 @@ namespace nil::crypto3::multiprecision {
     constexpr std::size_t lsb(T a) {
         return static_cast<big_uint<detail::get_bits<T>()>>(a).lsb();
     }
-    
+
     template<std::size_t Bits>
     constexpr bool bit_test(const big_uint<Bits> &a, std::size_t index) {
         return a.bit_test(index);
@@ -32,4 +35,12 @@ namespace nil::crypto3::multiprecision {
     constexpr bool bit_test(T a, std::size_t index) {
         return static_cast<big_uint<detail::get_bits<T>()>>(a).bit_test(index);
     }
-}    // namespace nil::crypto3::multiprecision
+
+    template<std::size_t Bits1, std::size_t Bits2>
+    constexpr void divide_qr(big_uint<Bits1> a, big_uint<Bits2> b, big_uint<Bits1> &q,
+                             big_uint<Bits1> &r) {
+        // TODO(ioxid): make this efficient by using private `divide`
+        q = a / b;
+        r = a % b;
+    }
+}  // namespace nil::crypto3::multiprecision
