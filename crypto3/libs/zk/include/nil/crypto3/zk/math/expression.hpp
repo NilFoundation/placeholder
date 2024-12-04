bbb4c0cf1bd26d48a42e4236ff205902eff7e3f0
Andrey Nefedov <ioxid@nil.foundation>
2024-12-04 10:58:19 +0000

    zk: remove boost::multiprecision remains

--- crypto3/libs/zk/include/nil/crypto3/zk/math/expression.hpp
+++ crypto3/libs/zk/include/nil/crypto3/zk/math/expression.hpp
@@ -113,7 +113,7 @@ namespace nil {
                     update_hash();
                 }
 
-                // Constructor for number<cpp_int_backend<...>>.
+                // Constructor for big_uint.
                 template<std::size_t Bits>
                 expression(const nil::crypto3::multiprecision::big_uint<Bits>& coeff)
                   : expr(term<VariableType>((assignment_type)coeff)) {
@@ -209,7 +209,7 @@ namespace nil {
 
                 // Every number type will be accepted here,
                 // if it can be converted to 'assignment_type'.
-                // This will include integral types and number<cpp_int_backend<...>>
+                // This will include integral types and big_uint
                 template<class NumberType>
                 term(const NumberType &field_val) : coeff(field_val) {
                     update_hash();
