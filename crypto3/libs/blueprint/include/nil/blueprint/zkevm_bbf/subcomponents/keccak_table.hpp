7cf8fa99f606340615a93d2389354217a70ac334
Daniel Kogtev <oclaw@nil.foundation>
2024-12-04 15:55:11 +0000

    fix rw trace deserialize

--- crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/subcomponents/keccak_table.hpp
+++ crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/subcomponents/keccak_table.hpp
@@ -87,6 +87,7 @@ namespace nil {
                             }
                             TYPE RLC_value = calculateRLC<FieldType>(msg, theta);
                             for( std::size_t block = 0; block < std::ceil(float(msg.size() + 1)/136); block++){
+                                assert(block_counter < max_blocks);
                                 if( block != std::ceil(float(msg.size() + 1)/136) - 1){
                                     is_last[block_counter] = 0;
                                 } else {
