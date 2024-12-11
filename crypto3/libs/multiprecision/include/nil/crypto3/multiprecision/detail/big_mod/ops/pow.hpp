0f1150de6ea95037401a5fb6418e6e21ca3ce123
Andrey Nefedov <ioxid@nil.foundation>
2024-12-11 13:36:32 +0000

    multiprecision: fix pow(..., INT_MIN)

--- crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/big_mod/ops/pow.hpp
+++ crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/big_mod/ops/pow.hpp
@@ -18,21 +18,27 @@
 
 #include "nil/crypto3/multiprecision/detail/big_mod/big_mod_impl.hpp"
 #include "nil/crypto3/multiprecision/detail/big_mod/ops/inverse.hpp"
+#include "nil/crypto3/multiprecision/detail/integer_ops_base.hpp"
+#include "nil/crypto3/multiprecision/detail/type_traits.hpp"
 
 namespace nil::crypto3::multiprecision {
     template<typename big_mod_t, typename T,
-             std::enable_if_t<detail::is_big_mod_v<big_mod_t> && std::numeric_limits<T>::is_integer,
+             std::enable_if_t<detail::is_big_mod_v<big_mod_t> && detail::is_unsigned_integer_v<T>,
                               int> = 0>
-    constexpr big_mod_t pow(big_mod_t b, T e) {
-        if constexpr (std::is_signed_v<T>) {
-            if (e < 0) {
-                b = inverse_extended_euclidean_algorithm(b);
-                // TODO(ioxid): this won't work for e == min()
-                e = -e;
-            }
-        }
+    constexpr big_mod_t pow(const big_mod_t &b, const T &e) {
         auto result = b;
-        result.ops().exp(result.raw_base(), b.raw_base(), std::move(e));
+        result.ops().exp(result.raw_base(), b.raw_base(), e);
         return result;
+    }
+
+    template<typename big_mod_t, typename T,
+             std::enable_if_t<detail::is_big_mod_v<big_mod_t> && detail::is_integer_v<T> &&
+                                  std::numeric_limits<T>::is_signed,
+                              int> = 0>
+    constexpr big_mod_t pow(const big_mod_t &b, const T &e) {
+        if (e < 0) {
+            return pow(inverse_extended_euclidean_algorithm(b), unsigned_abs(e));
+        }
+        return pow(b, static_cast<std::make_unsigned_t<T>>(e));
     }
 }  // namespace nil::crypto3::multiprecision
