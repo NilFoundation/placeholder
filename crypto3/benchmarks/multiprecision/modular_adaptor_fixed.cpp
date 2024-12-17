d3c394543a84b1ba547c0682aa1f44c6d6ac10aa
Andrey Nefedov <ioxid@nil.foundation>
2024-12-17 21:27:30 +0100

    multiprecision: add inverse benchmarks

--- crypto3/benchmarks/multiprecision/modular_adaptor_fixed.cpp
+++ crypto3/benchmarks/multiprecision/modular_adaptor_fixed.cpp
@@ -7,39 +7,22 @@
 // http://www.boost.org/LICENSE_1_0.txt
 //---------------------------------------------------------------------------//
 
-#include "nil/crypto3/multiprecision/boost_backends/modular/modular_params.hpp"
 #define BOOST_TEST_MODULE modular_fixed_multiprecision_test
 
 #define TEST_CPP_INT
 
-// Suddenly, BOOST_MP_ASSERT is NOT constexpr, and it is used in constexpr
-// functions throughout the boost, resulting to compilation errors on all
-// compilers in debug mode. We need to switch assertions off inside cpp_int
-// to make this code compile in debug mode. So we use this workaround to
-// turn off file 'boost/multiprecision/detail/assert.hpp' which contains
-// definition of BOOST_MP_ASSERT and BOOST_MP_ASSERT_MSG.
-#ifndef BOOST_MP_DETAIL_ASSERT_HPP
-    #define BOOST_MP_DETAIL_ASSERT_HPP
-    #define BOOST_MP_ASSERT(expr) ((void)0)
-    #define BOOST_MP_ASSERT_MSG(expr, msg) ((void)0)
-#endif
-
-#include <boost/test/unit_test.hpp>
-#include <boost/test/data/test_case.hpp>
 #include <boost/test/data/monomorphic.hpp>
-
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/unit_test.hpp>
 #include <iostream>
-
+#include <nil/crypto3/bench/benchmark.hpp>
 #include <nil/crypto3/multiprecision/boost_backends/cpp_int_modular.hpp>
 #include <nil/crypto3/multiprecision/boost_backends/cpp_int_modular/literals.hpp>
 #include <nil/crypto3/multiprecision/boost_backends/cpp_modular.hpp>
-
-#include <nil/crypto3/multiprecision/boost_backends/modular/modular_adaptor.hpp>
-#include <nil/crypto3/multiprecision/boost_backends/modular/modular_params_fixed.hpp>
-
 #include <nil/crypto3/multiprecision/boost_backends/inverse.hpp>
-
-#include <nil/crypto3/bench/benchmark.hpp>
+#include <nil/crypto3/multiprecision/boost_backends/modular/modular_adaptor.hpp>
+#include <nil/crypto3/multiprecision/boost_backends/modular/modular_params.hpp>
+#include <nil/crypto3/multiprecision/boost_backends/modular/modular_params_fixed.hpp>
 
 using namespace boost::multiprecision;
 
@@ -51,8 +34,10 @@ using boost::multiprecision::backends::modular_params_rt;
 using Backend = cpp_int_modular_backend<256>;
 using standart_number = boost::multiprecision::number<Backend>;
 
-constexpr standart_number modulus_odd = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f_cppui_modular256;
-constexpr standart_number modulus_even = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2e_cppui_modular256;
+constexpr standart_number modulus_odd =
+    0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f_cppui_modular256;
+constexpr standart_number modulus_even =
+    0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2e_cppui_modular256;
 constexpr backends::modular_params<Backend> params_odd{modulus_odd.backend()};
 constexpr backends::modular_params<Backend> params_even{modulus_even.backend()};
 
@@ -68,17 +53,23 @@ using modular_number_ct_odd = boost::multiprecision::number<modular_backend_ct_o
 using modular_number_ct_even = boost::multiprecision::number<modular_backend_ct_even>;
 using modular_number_rt = boost::multiprecision::number<modular_backend_rt>;
 
-constexpr standart_number x = 0xb5d724ce6f44c3c587867bbcb417e9eb6fa05e7e2ef029166568f14eb3161387_cppui_modular256;
+constexpr standart_number x =
+    0xb5d724ce6f44c3c587867bbcb417e9eb6fa05e7e2ef029166568f14eb3161387_cppui_modular256;
 constexpr modular_number_ct_odd x_mod_ct_odd = modular_backend_ct_odd(x.backend());
 constexpr modular_number_ct_even x_mod_ct_even = modular_backend_ct_even(x.backend());
-constexpr modular_number_rt x_mod_rt_odd = modular_backend_rt(x.backend(), modulus_odd.backend());
-constexpr modular_number_rt x_mod_rt_even = modular_backend_rt(x.backend(), modulus_even.backend());
+constexpr modular_number_rt x_mod_rt_odd =
+    modular_backend_rt(x.backend(), modulus_odd.backend());
+constexpr modular_number_rt x_mod_rt_even =
+    modular_backend_rt(x.backend(), modulus_even.backend());
 
-constexpr standart_number y = 0xad6e1fcc680392abfb075838eafa513811112f14c593e0efacb6e9d0d7770b4_cppui_modular256;
+constexpr standart_number y =
+    0xad6e1fcc680392abfb075838eafa513811112f14c593e0efacb6e9d0d7770b4_cppui_modular256;
 constexpr modular_number_ct_odd y_mod_ct_odd = modular_backend_ct_odd(y.backend());
 constexpr modular_number_ct_even y_mod_ct_even = modular_backend_ct_even(y.backend());
-constexpr modular_number_rt y_mod_rt_odd = modular_backend_rt(y.backend(), modulus_odd.backend());
-constexpr modular_number_rt y_mod_rt_even = modular_backend_rt(y.backend(), modulus_even.backend());
+constexpr modular_number_rt y_mod_rt_odd =
+    modular_backend_rt(y.backend(), modulus_odd.backend());
+constexpr modular_number_rt y_mod_rt_even =
+    modular_backend_rt(y.backend(), modulus_even.backend());
 
 BOOST_AUTO_TEST_SUITE(runtime_odd_tests)
 
@@ -88,12 +79,14 @@ BOOST_AUTO_TEST_CASE(montgomery_mul_perf_test) {
     const auto &mod_object = x_mod_rt_odd.backend().mod_data().get_mod_obj();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][runtime] montgomery_mul (direct call)",
-            [&]() {
-                mod_object.montgomery_mul(base_data, y_mod_rt_odd.backend().base_data(),
-                    std::integral_constant<bool, boost::multiprecision::backends::is_trivial_cpp_int_modular<Backend>::value>());
-                return base_data;
-            });
+        "[odd modulus][runtime] montgomery_mul (direct call)", [&]() {
+            mod_object.montgomery_mul(
+                base_data, y_mod_rt_odd.backend().base_data(),
+                std::integral_constant<
+                    bool, boost::multiprecision::backends::is_trivial_cpp_int_modular<
+                              Backend>::value>());
+            return base_data;
+        });
 
     std::cout << base_data << std::endl;
 }
@@ -104,11 +97,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_sub_perf_test) {
     auto x_modular = x_mod_rt_odd.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][runtime] modular_adaptor_backend_subtract",
-            [&]() {
-                eval_subtract(x_modular, y_mod_rt_odd.backend());
-                return x_modular;
-            });
+        "[odd modulus][runtime] modular_adaptor_backend_subtract", [&]() {
+            eval_subtract(x_modular, y_mod_rt_odd.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -120,11 +112,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_add_perf_test) {
     auto x_modular = x_mod_rt_odd.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][runtime] modular_adaptor_backend_add",
-            [&]() {
-                eval_add(x_modular, y_mod_rt_odd.backend());
-                return x_modular;
-            });
+        "[odd modulus][runtime] modular_adaptor_backend_add", [&]() {
+            eval_add(x_modular, y_mod_rt_odd.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -134,11 +125,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_mul_perf_test) {
     auto x_modular = x_mod_rt_odd.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][runtime] modular_adaptor_backend_multiply",
-            [&]() {
-                eval_multiply(x_modular, y_mod_rt_odd.backend());
-                return x_modular;
-            });
+        "[odd modulus][runtime] modular_adaptor_backend_multiply", [&]() {
+            eval_multiply(x_modular, y_mod_rt_odd.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -148,11 +138,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_number_mul_perf_test) {
     auto x = x_mod_rt_odd;
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][runtime] modular_adaptor_number_multiply",
-            [&]() {
-                x *= y_mod_rt_odd;
-                return x;
-            });
+        "[odd modulus][runtime] modular_adaptor_number_multiply", [&]() {
+            x *= y_mod_rt_odd;
+            return x;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x << std::endl;
@@ -168,12 +157,14 @@ BOOST_AUTO_TEST_CASE(montgomery_mul_perf_test) {
     const auto &mod_object = x_mod_ct_odd.backend().mod_data().get_mod_obj();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][compile time] montgomery_mul (direct call)",
-            [&]() {
-                mod_object.montgomery_mul(base_data, y_mod_ct_odd.backend().base_data(),
-                    std::integral_constant<bool, boost::multiprecision::backends::is_trivial_cpp_int_modular<Backend>::value>());
-                return base_data;
-            });
+        "[odd modulus][compile time] montgomery_mul (direct call)", [&]() {
+            mod_object.montgomery_mul(
+                base_data, y_mod_ct_odd.backend().base_data(),
+                std::integral_constant<
+                    bool, boost::multiprecision::backends::is_trivial_cpp_int_modular<
+                              Backend>::value>());
+            return base_data;
+        });
 
     std::cout << base_data << std::endl;
 }
@@ -184,11 +175,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_sub_perf_test) {
     auto x_modular = x_mod_ct_odd.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][compile time] modular_adaptor_backend_subtract",
-            [&]() {
-                eval_subtract(x_modular, y_mod_ct_odd.backend());
-                return x_modular;
-            });
+        "[odd modulus][compile time] modular_adaptor_backend_subtract", [&]() {
+            eval_subtract(x_modular, y_mod_ct_odd.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -200,11 +190,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_add_perf_test) {
     auto x_modular = x_mod_ct_odd.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][compile time] modular_adaptor_backend_add",
-            [&]() {
-                eval_add(x_modular, y_mod_ct_odd.backend());
-                return x_modular;
-            });
+        "[odd modulus][compile time] modular_adaptor_backend_add", [&]() {
+            eval_add(x_modular, y_mod_ct_odd.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -214,11 +203,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_mul_perf_test) {
     auto x_modular = x_mod_ct_odd.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][compile time] modular_adaptor_backend_multiply",
-            [&]() {
-                eval_multiply(x_modular, y_mod_ct_odd.backend());
-                return x_modular;
-            });
+        "[odd modulus][compile time] modular_adaptor_backend_multiply", [&]() {
+            eval_multiply(x_modular, y_mod_ct_odd.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -228,11 +216,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_number_mul_perf_test) {
     auto x = x_mod_ct_odd;
 
     nil::crypto3::bench::run_benchmark<>(
-            "[odd modulus][compile time] modular_adaptor_number_multiply",
-            [&]() {
-                x *= y_mod_ct_odd;
-                return x;
-            });
+        "[odd modulus][compile time] modular_adaptor_number_multiply", [&]() {
+            x *= y_mod_ct_odd;
+            return x;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x << std::endl;
@@ -248,11 +235,10 @@ BOOST_AUTO_TEST_CASE(barrett_mul_perf_test) {
     const auto &mod_object = x_mod_rt_even.backend().mod_data().get_mod_obj();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][runtime] regular_mul (direct call)",
-            [&]() {
-                mod_object.regular_mul(base_data, y_mod_rt_even.backend().base_data());
-                return base_data;
-            });
+        "[even modulus][runtime] regular_mul (direct call)", [&]() {
+            mod_object.regular_mul(base_data, y_mod_rt_even.backend().base_data());
+            return base_data;
+        });
 
     std::cout << base_data << std::endl;
 }
@@ -263,11 +249,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_sub_perf_test) {
     auto x_modular = x_mod_rt_even.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][runtime] modular_adaptor_backend_subtract",
-            [&]() {
-                eval_subtract(x_modular, y_mod_rt_even.backend());
-                return x_modular;
-            });
+        "[even modulus][runtime] modular_adaptor_backend_subtract", [&]() {
+            eval_subtract(x_modular, y_mod_rt_even.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -279,11 +264,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_add_perf_test) {
     auto x_modular = x_mod_rt_even.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][runtime] modular_adaptor_backend_add",
-            [&]() {
-                eval_add(x_modular, y_mod_rt_even.backend());
-                return x_modular;
-            });
+        "[even modulus][runtime] modular_adaptor_backend_add", [&]() {
+            eval_add(x_modular, y_mod_rt_even.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -293,11 +277,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_mul_perf_test) {
     auto x_modular = x_mod_rt_even.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][runtime] modular_adaptor_backend_multiply",
-            [&]() {
-                eval_multiply(x_modular, y_mod_rt_even.backend());
-                return x_modular;
-            });
+        "[even modulus][runtime] modular_adaptor_backend_multiply", [&]() {
+            eval_multiply(x_modular, y_mod_rt_even.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -307,11 +290,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_number_mul_perf_test) {
     auto x = x_mod_rt_even;
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][runtime] modular_adaptor_number_multiply",
-            [&]() {
-                x *= y_mod_rt_even;
-                return x;
-            });
+        "[even modulus][runtime] modular_adaptor_number_multiply", [&]() {
+            x *= y_mod_rt_even;
+            return x;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x << std::endl;
@@ -327,11 +309,10 @@ BOOST_AUTO_TEST_CASE(barrett_mul_perf_test) {
     const auto &mod_object = x_mod_ct_even.backend().mod_data().get_mod_obj();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][compile time] regular_mul (direct call)",
-            [&]() {
-                mod_object.regular_mul(base_data, y_mod_ct_even.backend().base_data());
-                return base_data;
-            });
+        "[even modulus][compile time] regular_mul (direct call)", [&]() {
+            mod_object.regular_mul(base_data, y_mod_ct_even.backend().base_data());
+            return base_data;
+        });
 
     std::cout << base_data << std::endl;
 }
@@ -342,11 +323,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_sub_perf_test) {
     auto x_modular = x_mod_ct_even.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][compile time] modular_adaptor_backend_subtract",
-            [&]() {
-                eval_subtract(x_modular, y_mod_ct_even.backend());
-                return x_modular;
-            });
+        "[even modulus][compile time] modular_adaptor_backend_subtract", [&]() {
+            eval_subtract(x_modular, y_mod_ct_even.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -358,11 +338,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_add_perf_test) {
     auto x_modular = x_mod_ct_even.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][compile time] modular_adaptor_backend_add",
-            [&]() {
-                eval_add(x_modular, y_mod_ct_even.backend());
-                return x_modular;
-            });
+        "[even modulus][compile time] modular_adaptor_backend_add", [&]() {
+            eval_add(x_modular, y_mod_ct_even.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -372,11 +351,10 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_backend_mul_perf_test) {
     auto x_modular = x_mod_ct_even.backend();
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][compile time] modular_adaptor_backend_multiply",
-            [&]() {
-                eval_multiply(x_modular, y_mod_ct_even.backend());
-                return x_modular;
-            });
+        "[even modulus][compile time] modular_adaptor_backend_multiply", [&]() {
+            eval_multiply(x_modular, y_mod_ct_even.backend());
+            return x_modular;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x_modular << std::endl;
@@ -386,14 +364,45 @@ BOOST_AUTO_TEST_CASE(modular_adaptor_number_mul_perf_test) {
     auto x = x_mod_ct_even;
 
     nil::crypto3::bench::run_benchmark<>(
-            "[even modulus][compile time] modular_adaptor_number_multiply",
-            [&]() {
-                x *= y_mod_ct_even;
-                return x;
-            });
+        "[even modulus][compile time] modular_adaptor_number_multiply", [&]() {
+            x *= y_mod_ct_even;
+            return x;
+        });
 
     // Print something so the whole computation is not optimized out.
     std::cout << x << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(compile_time_inverse_tests)
+
+BOOST_AUTO_TEST_CASE(inverse_extended_euclidean_algorithm_test) {
+    auto x_modular = x_mod_ct_odd;
+
+    nil::crypto3::bench::run_benchmark<>(
+        "[odd modulus][compile time] inverse_extended_euclidean_algorithm_test", [&]() {
+            x_modular = inverse_extended_euclidean_algorithm(x_modular);
+            ++x_modular;
+            return x_modular;
+        });
+
+    // Print something so the whole computation is not optimized out.
+    std::cout << x_modular << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(inverse_mod_test) {
+    auto x_modular = x_mod_ct_odd;
+
+    nil::crypto3::bench::run_benchmark<>("[odd modulus][compile time] inverse_mod",
+                                         [&]() {
+                                             x_modular = inverse_mod(x_modular);
+                                             ++x_modular;
+                                             return x_modular;
+                                         });
+
+    // Print something so the whole computation is not optimized out.
+    std::cout << x_modular << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
