29f2fd2e31d881ffa887d8a7810629006519b1d4
Andrey Nefedov <ioxid@nil.foundation>
2024-12-18 13:36:40 +0000

    multiprecision: clean up formatting

--- crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/big_uint.hpp
+++ crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/big_uint.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "nil/crypto3/multiprecision/detail/big_uint/big_uint_impl.hpp"      // IWYU pragma: export
-#include "nil/crypto3/multiprecision/detail/big_uint/limits.hpp"             // IWYU pragma: export
-#include "nil/crypto3/multiprecision/detail/big_uint/ops/gcd_inverse.hpp"    // IWYU pragma: export
-#include "nil/crypto3/multiprecision/detail/big_uint/ops/jacobi.hpp"         // IWYU pragma: export
-#include "nil/crypto3/multiprecision/detail/big_uint/ops/powm.hpp"           // IWYU pragma: export
-#include "nil/crypto3/multiprecision/detail/big_uint/ops/ressol.hpp"         // IWYU pragma: export
-#include "nil/crypto3/multiprecision/detail/big_uint/ops/wnaf.hpp"           // IWYU pragma: export
+#include "nil/crypto3/multiprecision/detail/big_uint/big_uint_impl.hpp"    // IWYU pragma: export
+#include "nil/crypto3/multiprecision/detail/big_uint/limits.hpp"           // IWYU pragma: export
+#include "nil/crypto3/multiprecision/detail/big_uint/ops/gcd_inverse.hpp"  // IWYU pragma: export
+#include "nil/crypto3/multiprecision/detail/big_uint/ops/jacobi.hpp"       // IWYU pragma: export
+#include "nil/crypto3/multiprecision/detail/big_uint/ops/powm.hpp"         // IWYU pragma: export
+#include "nil/crypto3/multiprecision/detail/big_uint/ops/ressol.hpp"       // IWYU pragma: export
+#include "nil/crypto3/multiprecision/detail/big_uint/ops/wnaf.hpp"         // IWYU pragma: export
 
 namespace nil::crypto3::multiprecision {
     using uint128_t = big_uint<128>;
