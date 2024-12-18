91a232642aed77c6960793752b400bf33b73b371
Andrey Nefedov <ioxid@nil.foundation>
2024-12-18 12:39:44 +0000

    multiprecision: throw on overflowing import

--- crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/big_uint/ops/import_export.hpp
+++ crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/big_uint/ops/import_export.hpp
@@ -8,25 +8,22 @@
 
 // IWYU pragma: private; include "nil/crypto3/multiprecision/big_uint.hpp"
 
+#include <algorithm>
 #include <climits>
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
 #include <iterator>
 #include <limits>
+#include <stdexcept>
 #include <type_traits>
 
-#include "nil/crypto3/multiprecision/detail/assert.hpp"
 #include "nil/crypto3/multiprecision/detail/big_uint/big_uint_impl.hpp"
 #include "nil/crypto3/multiprecision/detail/big_uint/storage.hpp"
 #include "nil/crypto3/multiprecision/detail/endian.hpp"
 
 namespace nil::crypto3::multiprecision {
     namespace detail {
-        /* This specialization is used when assigning `chunk_bits`
-         * of `bits` into `val` at `bit_location` in case where `val`
-         * is larger than one limb (machine word).
-         */
         template<std::size_t Bits, typename Unsigned>
         void assign_bits(big_uint<Bits>& val, Unsigned bits, std::size_t bit_location,
                          std::size_t chunk_bits) {
@@ -39,12 +36,10 @@ namespace nil::crypto3::multiprecision {
 
             limb_type value = static_cast<limb_type>(bits & mask) << shift;
             if (value) {
-                // TODO(ioxid): when should we throw?
-                // We are ignoring any bits that will not fit into the number.
-                // We are not throwing, we will use as many bits from the input as we need to.
-                if (val.limb_count() > limb) {
-                    val.limbs()[limb] |= value;
+                if (limb >= val.limb_count()) {
+                    throw std::overflow_error("import_bits: overflow");
                 }
+                val.limbs()[limb] |= value;
             }
 
             /* If some extra bits need to be assigned to the next limb */
@@ -57,35 +52,6 @@ namespace nil::crypto3::multiprecision {
                     assign_bits(val, extra_bits, bit_location, chunk_bits);
                 }
             }
-        }
-
-        /* This specialization is used when assigning `chunk_bits`
-         * of `bits` into `val` at `bit_location` in case where `val`
-         * fits into one limb (machine word).
-         */
-        template<std::size_t Bits, typename Unsigned>
-        void assign_bits(big_uint<Bits>& val, Unsigned bits, std::size_t bit_location,
-                         std::size_t chunk_bits,
-                         const std::integral_constant<bool, true>& /*unused*/) {
-            using limb_type = typename big_uint<Bits>::limb_type;
-            //
-            // Check for possible overflow.
-            //
-            NIL_CO3_MP_ASSERT(!((bit_location >= limb_bits) && bits));
-
-            limb_type mask = chunk_bits >= limb_bits
-                                 ? ~static_cast<limb_type>(0u)
-                                 : (static_cast<limb_type>(1u) << chunk_bits) - 1;
-            limb_type value = (static_cast<limb_type>(bits) & mask) << bit_location;
-            *val.limbs() |= value;
-
-            //
-            // Check for overflow bits:
-            //
-            bit_location = limb_bits - bit_location;
-
-            NIL_CO3_MP_ASSERT(
-                !((bit_location < sizeof(bits) * CHAR_BIT) && (bits >> bit_location)));
         }
 
         template<std::size_t Bits>
@@ -123,10 +89,6 @@ namespace nil::crypto3::multiprecision {
             size_type limbs = std::distance(i, j);
             size_type bits = limbs * chunk_size;
 
-            // TODO(ioxid): this breaks marshalling tests
-            // We should throw an exception here. And check that excess bits are zero.
-            // NIL_CO3_MP_ASSERT(bits <= Bits);
-
             difference_type bit_location = msv_first ? bits - chunk_size : 0;
             difference_type bit_location_change =
                 msv_first ? -static_cast<difference_type>(chunk_size) : chunk_size;
@@ -137,35 +99,43 @@ namespace nil::crypto3::multiprecision {
                 bit_location += bit_location_change;
             }
 
-            // This will remove the upper bits using upper_limb_mask.
-            newval.normalize();
+            if (newval.normalize()) {
+                throw std::overflow_error("import_bits: overflow");
+            }
 
-            result = std::move(newval);
+            result = newval;
             return result;
         }
 
         template<std::size_t Bits, typename T>
-        inline big_uint<Bits>& import_bits_fast(big_uint<Bits>& result, T* i, T* j,
-                                                std::size_t chunk_size = 0) {
-            std::size_t byte_len = (j - i) * (chunk_size ? chunk_size / CHAR_BIT : sizeof(*i));
+        inline big_uint<Bits>& import_bits_fast(big_uint<Bits>& result, T* i, T* j) {
+            std::size_t byte_len = (j - i) * sizeof(*i);
             std::size_t limb_len = byte_len / sizeof(limb_type);
             if (byte_len % sizeof(limb_type)) {
                 ++limb_len;
             }
 
-            NIL_CO3_MP_ASSERT(result.limb_count() > limb_len);
+            std::size_t copy_len = (std::min)(byte_len, result.limb_count() * sizeof(limb_type));
 
-            result.limbs()[result.limb_count() - 1] = 0u;
-            std::memcpy(result.limbs(), i,
-                        (std::min)(byte_len, result.limb_count() * sizeof(limb_type)));
+            if (std::any_of(reinterpret_cast<const char*>(i), reinterpret_cast<const char*>(j),
+                            [](char c) { return c != 0; })) {
+                throw std::overflow_error("import_bits: overflow");
+            }
 
-            // This is probably unneeded, but let it stay for now.
-            result.normalize();
+            std::memcpy(result.limbs(), i, copy_len);
+            std::memset(result.limbs() + copy_len, 0,
+                        result.limb_count() * sizeof(limb_type) - copy_len);
+
+            if (result.normalize()) {
+                throw std::overflow_error("import_bits: overflow");
+            }
+
             return result;
         }
     }  // namespace detail
 
-    template<std::size_t Bits, typename Iterator>
+    template<std::size_t Bits, typename Iterator,
+             std::enable_if_t<!std::is_pointer_v<Iterator>, int> = 0>
     inline big_uint<Bits>& import_bits(big_uint<Bits>& val, Iterator i, Iterator j,
                                        std::size_t chunk_size = 0, bool msv_first = true) {
         return detail::import_bits_generic(val, i, j, chunk_size, msv_first);
@@ -176,7 +146,7 @@ namespace nil::crypto3::multiprecision {
                                        bool msv_first = true) {
 #if NIL_CO3_MP_ENDIAN_LITTLE_BYTE
         if (((chunk_size % CHAR_BIT) == 0) && !msv_first && (sizeof(*i) * CHAR_BIT == chunk_size)) {
-            return detail::import_bits_fast(val, i, j, chunk_size);
+            return detail::import_bits_fast(val, i, j);
         }
 #endif
         return detail::import_bits_generic(val, i, j, chunk_size, msv_first);
@@ -203,7 +173,7 @@ namespace nil::crypto3::multiprecision {
             *out = detail::extract_bits(val, bit_location, chunk_size);
             ++out;
             bit_location += bit_step;
-        } while ((bit_location >= 0) && (bit_location < static_cast<int>(bitcount)));
+        } while ((bit_location >= 0) && (bit_location < static_cast<std::ptrdiff_t>(bitcount)));
 
         return out;
     }
