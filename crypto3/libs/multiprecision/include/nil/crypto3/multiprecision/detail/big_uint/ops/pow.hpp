f06444b4f1d6a4e81b6894ae063bc9c29c3dd9b4
Andrey Nefedov <ioxid@nil.foundation>
2024-12-19 14:44:49 +0000

    multiprecision: add custom and boost tests and fix bugs

--- crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/big_uint/ops/pow.hpp
+++ crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/big_uint/ops/pow.hpp
@@ -15,20 +15,33 @@
 
 #include <type_traits>
 
-#include "nil/crypto3/multiprecision/detail/big_mod/big_mod_impl.hpp"
-#include "nil/crypto3/multiprecision/detail/big_mod/ops/pow.hpp"
 #include "nil/crypto3/multiprecision/detail/big_uint/big_uint_impl.hpp"
+#include "nil/crypto3/multiprecision/detail/integer_ops_base.hpp"
 
 namespace nil::crypto3::multiprecision {
-    template<typename T1, typename T2, typename T3,
+    template<typename T1, typename T2,
              std::enable_if_t<detail::is_integral_v<std::decay_t<T1>> &&
-                                  detail::is_integral_v<std::decay_t<T2>> &&
-                                  detail::is_integral_v<std::decay_t<T3>>,
+                                  detail::is_integral_v<std::decay_t<T2>>,
                               int> = 0>
-    constexpr std::decay_t<T3> powm(T1 &&b, T2 &&e, T3 &&m) {
-        using big_mod_t = big_mod_rt<
-            std::decay_t<decltype(detail::as_big_uint(detail::unsigned_or_throw(m)))>::Bits>;
-        return static_cast<std::decay_t<T3>>(
-            pow(big_mod_t(std::forward<T1>(b), std::forward<T3>(m)), std::forward<T2>(e)).base());
+    constexpr std::decay_t<T1> pow(T1 b, T2 e) {
+        if (is_zero(e)) {
+            return 1u;
+        }
+
+        T1 res = 1u;
+
+        while (true) {
+            bool lsb = bit_test(e, 0u);
+            e >>= 1u;
+            if (lsb) {
+                res *= b;
+                if (is_zero(e)) {
+                    break;
+                }
+            }
+            b *= b;
+        }
+
+        return res;
     }
 }  // namespace nil::crypto3::multiprecision
