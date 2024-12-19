9d77efec46421b7bcbfebf92319792bc91bb6ab1
Andrey Nefedov <ioxid@nil.foundation>
2024-12-19 01:37:09 +0000

    multiprecision: update and fix copyrights

--- crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/config.hpp
+++ crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/config.hpp
@@ -1,3 +1,12 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2021 Matt Borland
+// Copyright (c) 2024 Andrey Nefedov <ioxid@nil.foundation>
+//
+// Distributed under the Boost Software License, Version 1.0
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//---------------------------------------------------------------------------//
+
 #pragma once
 
 #if !defined(NIL_CO3_MP_FORCEINLINE)
