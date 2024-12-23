7fb4283c217bc90cff04407a912a9f59d18bb3d5
Daniel Kogtev <oclaw@nil.foundation>
2024-12-23 17:07:56 +0000

    assigner: add trace match check

---
+++ proof-producer/libs/assigner/include/nil/proof-generator/assigner/options.hpp
@@ -0,0 +1,15 @@
+#ifndef PROOF_GENERATOR_LIBS_ASSIGNER_OPTIONS_HPP_
+#define PROOF_GENERATOR_LIBS_ASSIGNER_OPTIONS_HPP_
+
+namespace nil {
+    namespace proof_generator {
+
+        struct AssignerOptions {
+            bool ignore_index_mismatch{};
+       };
+
+
+    } // proof_generator
+} // namespace nil
+
+#endif // PROOF_GENERATOR_LIBS_ASSIGNER_OPTIONS_HPP_
