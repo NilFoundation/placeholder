74d81bd857b242899619608c1f1c8e293fc66a32
Iluvmagick <d.tabalin@nil.foundation>
2025-01-14 05:20:15 +0400

    Blueprint disable unnecessary test and mac platform bug fixes.

--- crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/endian.hpp
+++ crypto3/libs/multiprecision/include/nil/crypto3/multiprecision/detail/endian.hpp
@@ -9,38 +9,30 @@
 
 #pragma once
 
-#include <bits/endian.h>
+#include <boost/predef/other/endian.h>
 
 #define NIL_CO3_MP_ENDIAN_BIG_BYTE 0
 #define NIL_CO3_MP_ENDIAN_BIG_WORD 0
 #define NIL_CO3_MP_ENDIAN_LITTLE_BYTE 0
 #define NIL_CO3_MP_ENDIAN_LITTLE_WORD 0
 
-#if defined(__BYTE_ORDER)
-#if defined(__BIG_ENDIAN) && (__BYTE_ORDER == __BIG_ENDIAN)
-#undef NIL_CO3_MP_ENDIAN_BIG_BYTE
-#define NIL_CO3_MP_ENDIAN_BIG_BYTE 1
+#if defined(BOOST_ENDIAN_BIG_BYTE)
+    #undef NIL_CO3_MP_ENDIAN_BIG_BYTE
+    #define NIL_CO3_MP_ENDIAN_BIG_BYTE 1
 #endif
-#if defined(__LITTLE_ENDIAN) && (__BYTE_ORDER == __LITTLE_ENDIAN)
-#undef NIL_CO3_MP_ENDIAN_LITTLE_BYTE
-#define NIL_CO3_MP_ENDIAN_LITTLE_BYTE 1
+
+#if defined(BOOST_ENDIAN_LITTLE_BYTE)
+    #undef NIL_CO3_MP_ENDIAN_LITTLE_BYTE
+    #define NIL_CO3_MP_ENDIAN_LITTLE_BYTE 1
 #endif
-#if defined(__PDP_ENDIAN) && (__BYTE_ORDER == __PDP_ENDIAN)
-#undef NIL_CO3_MP_ENDIAN_LITTLE_WORD
-#define NIL_CO3_MP_ENDIAN_LITTLE_WORD 1
+
+#if defined(BOOST_ENDIAN_BIG_WORD)
+    #undef NIL_CO3_MP_ENDIAN_BIG_WORD
+    #define NIL_CO3_MP_ENDIAN_BIG_WORD 1
 #endif
+
+#if defined(BOOST_ENDIAN_LITTLE_WORD)
+    #undef NIL_CO3_MP_ENDIAN_LITTLE_WORD
+    #define NIL_CO3_MP_ENDIAN_LITTLE_WORD 1
 #endif
-#if !defined(__BYTE_ORDER) && defined(_BYTE_ORDER)
-#if defined(_BIG_ENDIAN) && (_BYTE_ORDER == _BIG_ENDIAN)
-#undef NIL_CO3_MP_ENDIAN_BIG_BYTE
-#define NIL_CO3_MP_ENDIAN_BIG_BYTE 1
-#endif
-#if defined(_LITTLE_ENDIAN) && (_BYTE_ORDER == _LITTLE_ENDIAN)
-#undef NIL_CO3_MP_ENDIAN_LITTLE_BYTE
-#define NIL_CO3_MP_ENDIAN_LITTLE_BYTE 1
-#endif
-#if defined(_PDP_ENDIAN) && (_BYTE_ORDER == _PDP_ENDIAN)
-#undef NIL_CO3_MP_ENDIAN_LITTLE_WORD
-#define NIL_CO3_MP_ENDIAN_LITTLE_WORD 1
-#endif
-#endif
+
