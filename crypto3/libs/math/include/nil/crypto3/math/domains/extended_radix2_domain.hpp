366c27ee7cd97610fddcfda471b4ebb84cc3e07f
Vasiliy Olekhov <vasiliy.olekhov@nil.foundation>
2025-01-28 10:33:32 +0200

    comments added

--- crypto3/libs/math/include/nil/crypto3/math/domains/extended_radix2_domain.hpp
+++ crypto3/libs/math/include/nil/crypto3/math/domains/extended_radix2_domain.hpp
@@ -72,12 +72,10 @@ namespace nil {
                     if (m <= 1)
                         throw std::invalid_argument("extended_radix2(): expected m > 1");
 
-                    if (!std::is_same<field_value_type, std::complex<double>>::value) {
-                        const std::size_t logm = static_cast<std::size_t>(std::ceil(std::log2(m)));
-                        if (logm != (fields::arithmetic_params<FieldType>::s + 1))
-                            throw std::invalid_argument(
+                    const std::size_t logm = static_cast<std::size_t>(std::ceil(std::log2(m)));
+                    if (logm != (fields::arithmetic_params<FieldType>::s + 1))
+                        throw std::invalid_argument(
                                 "extended_radix2(): expected logm == fields::arithmetic_params<FieldType>::s + 1");
-                    }
                 }
 
                 void fft(std::vector<value_type> &a) override {
