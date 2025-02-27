26e27b1249e5e57398af0b252ead53b9c7d91a21
Antoine Cyr <acyrntoine@gmail.com>
2025-02-27 14:11:40 -0500

    memory opcode gas calculation

--- crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/opcodes/mload.hpp
+++ crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/opcodes/mload.hpp
@@ -24,10 +24,12 @@
 
 #pragma once
 
-#include <numeric>
 #include <algorithm>
+#include <numeric>
 
 #include <nil/blueprint/zkevm/zkevm_word.hpp>
+#include <nil/blueprint/zkevm_bbf/subcomponents/memory_cost.hpp>
+#include <nil/blueprint/zkevm_bbf/subcomponents/word_size.hpp>
 #include <nil/blueprint/zkevm_bbf/types/opcode.hpp>
 
 namespace nil {
@@ -44,92 +46,74 @@ namespace nil {
                 using generic_component<FieldType, stage>::constrain;
                 using generic_component<FieldType, stage>::lookup;
                 using generic_component<FieldType, stage>::lookup_table;
-            public:
-                using typename generic_component<FieldType,stage>::TYPE;
 
-                zkevm_mload_bbf(context_type &context_object, const opcode_input_type<FieldType, stage> &current_state):
-                    generic_component<FieldType,stage>(context_object, false)
-                {
-                    TYPE addr;
-                    TYPE addr1;
-                    TYPE addr_mod;
-                    TYPE addr_mod31;
-                    TYPE addr_words;
-                    TYPE memory_mod;
-                    TYPE memory_mod31;
-                    TYPE memory_words;
-                    TYPE is_memory_size_changed;
-                    TYPE new_memory_size;
+              public:
+                using typename generic_component<FieldType, stage>::TYPE;
+
+                zkevm_mload_bbf(context_type &context_object,
+                                const opcode_input_type<FieldType, stage> &current_state)
+                    : generic_component<FieldType, stage>(context_object, false) {
+                    using Word_Size = typename bbf::word_size<FieldType, stage>;
+                    using Memory_Cost = typename bbf::memory_cost<FieldType, stage>;
+                    TYPE offset, length, current_mem, next_mem, memory_expansion_cost,
+                        memory_expansion_size, S;
                     std::vector<TYPE> bytes(32);
-                    if constexpr( stage == GenerationStage::ASSIGNMENT ){
+                    length = 32;
+                    if constexpr (stage == GenerationStage::ASSIGNMENT) {
                         auto address = w_to_16(current_state.stack_top())[15];
-                        for( std::size_t i = 0; i < 32; i++){
+                        offset = address;
+                        current_mem = current_state.memory_size;
+                        next_mem = std::max(offset + length, current_mem);
+                        S = next_mem > current_mem;
+                        for (std::size_t i = 0; i < 32; i++) {
                             auto b = w_to_8(current_state.memory(address + i))[31];
                             bytes[i] = TYPE(b);
                         }
-                        addr = address;
-                        addr1 = address;
-                        // addr + 31 -- maximum address
-                        // memory_size -- previous memory_size
-                        // if ( memory_size < addr + 31 ) then
-                        //    new_memory_size = std::ceil(float(addr + 31) / 32) * 32
-                        // else
-                        //    new_memory_size = memory_size
-                        // So, we have | addr | std::ceil(addr + 31) % 32 | (addr + 31) / 32 |
-                        std::size_t last_address = address + 31;
-                        addr_mod = last_address % 32;
-                        addr_mod31 = 31 - addr_mod;
-                        addr_words = last_address % 32 == 0? last_address/32 : last_address / 32 + 1;
-                        memory_mod = current_state.memory_size %32;
-                        memory_mod31 = 31 - memory_mod;
-                        memory_words = current_state.memory_size %32 == 0? current_state.memory_size/32 : current_state.memory_size / 32 + 1;
-
-                        std::cout << "\tMemory words = " << memory_words
-                            <<  " memory size = " << current_state.memory_size
-                            << " memory mod = " << memory_mod
-                            << " address = " << address
-                            << " last_address = " << last_address
-                            << " addr_mod = " << addr_mod
-                            << " addr_words = " << addr_words
-                            << std::endl;
-                        is_memory_size_changed = (addr_words > memory_words);
-                        if( is_memory_size_changed != 0 ) {
-                            std::cout << "\tMEMORY SIZE CHANGED "  << addr_words << " > " << memory_words << std::endl;
-                        } else {
-                            std::cout << "\tMEMORY SIZE NOT CHANGED " << addr_words << " <= " << memory_words << std::endl;
-                        }
-                    }
-                    for( std::size_t i = 0; i < 16; i++ ){
-                        allocate(bytes[i], i+16, 0);
-                        allocate(bytes[i+16], i+16, 1);
                     }
 
-                    allocate(addr, 0, 0);
-                    allocate(addr1, 0, 1);
-                    allocate(addr_mod, 1, 0);
-                    allocate(addr_mod31 , 2, 0);
-                    allocate(addr_words, 3, 0);
-                    allocate(memory_mod, 4, 0);
-                    allocate(memory_mod31, 5, 0);
-                    allocate(memory_words, 6, 0);
-                    allocate(is_memory_size_changed, 7, 0);
+                    for (std::size_t i = 0; i < 16; i++) {
+                        allocate(bytes[i], i + 16, 0);
+                        allocate(bytes[i + 16], i + 16, 1);
+                    }
 
-                    constrain( addr_mod + addr_mod31 - 31);
-                    constrain( memory_mod + memory_mod31 - 31);
-                    if constexpr( stage == GenerationStage::CONSTRAINTS ){
-                        constrain((addr + 31 - 32 * addr_words)*(32 * addr_words - 32 + addr_mod - addr - 31));
-                        constrain( addr_mod * (32 * addr_words - 32 + addr_mod - addr - 31));
-                        constrain((current_state.memory_size(0) - 32 * memory_words)*(32 * memory_words - 32 - current_state.memory_size(0) + memory_mod));
-                        constrain( memory_mod * (32 * memory_words - 32 - current_state.memory_size(0) + memory_mod));
-                        new_memory_size = is_memory_size_changed * addr_words * 32 + (1 - is_memory_size_changed) * current_state.memory_size(0);
-                        TYPE old_memory_gas_cost =  3 * memory_words;   // TODO: check and test with large memory consumption
-                        TYPE new_memory_gas_cost =  3 * addr_words;     // TODO: check and test with large memory consumption
+                    allocate(offset, 32, 0);
+                    allocate(length, 33, 0);
+                    allocate(current_mem, 34, 0);
+                    allocate(next_mem, 35, 0);
+                    allocate(S, 36, 0);
+                    constrain(S * (S - 1));
+                    constrain(S * (next_mem - offset - length) +
+                              (1 - S) * (next_mem - current_mem));
 
-                        constrain(current_state.pc_next() - current_state.pc(1) - 1);                     // PC transition
-                        //constrain(current_state.gas(0) - current_state.gas_next() - 3 - is_memory_size_changed * (new_memory_gas_cost - old_memory_gas_cost));               // GAS transition
-                        constrain(current_state.stack_size(1) - current_state.stack_size_next());         // stack_size transition
-                        //constrain(new_memory_size - current_state.memory_size_next());                    // memory_size transition
-                        constrain(current_state.rw_counter_next() - current_state.rw_counter(1) - 34);    // rw_counter transition
+                    allocate(memory_expansion_cost, 37, 0);
+                    allocate(memory_expansion_size, 38, 0);
+                    std::vector<std::size_t> memory_cost_lookup_area = {42, 43, 44,
+                                                                        45, 46, 47};
+                    context_type current_memory_ct =
+                        context_object.subcontext(memory_cost_lookup_area, 0, 1);
+                    context_type next_memory_ct =
+                        context_object.subcontext(memory_cost_lookup_area, 1, 1);
+                    Memory_Cost current_memory =
+                        Memory_Cost(current_memory_ct, current_mem);
+                    Memory_Cost next_memory = Memory_Cost(next_memory_ct, next_mem);
+                    memory_expansion_cost = next_memory.cost - current_memory.cost;
+                    memory_expansion_size =
+                        (next_memory.word_size - current_memory.word_size) * 32;
+
+                    if constexpr (stage == GenerationStage::CONSTRAINTS) {
+                        constrain(current_state.pc_next() - current_state.pc(1) -
+                                  1);  // PC transition
+                        constrain(current_state.gas(0) - current_state.gas_next() - 3 -
+                                  memory_expansion_cost);  // GAS transition
+                        constrain(
+                            current_state.stack_size(1) -
+                            current_state.stack_size_next());  // stack_size transition
+                        constrain(current_state.memory_size_next() -
+                                  current_state.memory_size(0) -
+                                  memory_expansion_size);  // memory_size transition
+                        constrain(current_state.rw_counter_next() -
+                                  current_state.rw_counter(1) -
+                                  34);  // rw_counter transition
 
                         auto V_128 = chunks8_to_chunks128<TYPE>(bytes);
                         std::vector<TYPE> tmp;
@@ -137,42 +121,44 @@ namespace nil {
                             TYPE(rw_op_to_num(rw_operation_type::stack)),
                             current_state.call_id(1),
                             current_state.stack_size(1) - 1,
-                            TYPE(0),                                               // storage_key_hi
-                            TYPE(0),                                               // storage_key_lo
-                            TYPE(0),                                               // field
+                            TYPE(0),  // storage_key_hi
+                            TYPE(0),  // storage_key_lo
+                            TYPE(0),  // field
                             current_state.rw_counter(1),
-                            TYPE(0),                                               // is_write
-                            TYPE(0),                                               // hi bytes are 0
-                            addr                                                   // addr is smaller than maximum contract size
+                            TYPE(0),  // is_write
+                            TYPE(0),  // hi bytes are 0
+                            offset    // offset is smaller than maximum contract size
                         };
                         lookup(tmp, "zkevm_rw");
-                        for( std::size_t i = 0; i < 32; i++ ){
-                            if( i < 16 ){
+                        for (std::size_t i = 0; i < 32; i++) {
+                            if (i < 16) {
                                 tmp = {
                                     TYPE(rw_op_to_num(rw_operation_type::memory)),
                                     current_state.call_id(0),
-                                    addr + i,
-                                    TYPE(0),                                               // storage_key_hi
-                                    TYPE(0),                                               // storage_key_lo
-                                    TYPE(0),                                               // field
+                                    offset + i,
+                                    TYPE(0),  // storage_key_hi
+                                    TYPE(0),  // storage_key_lo
+                                    TYPE(0),  // field
                                     current_state.rw_counter(0) + i + 1,
-                                    TYPE(0),                                               // is_write
-                                    TYPE(0),                                               // hi bytes are 0
-                                    bytes[i]                                               // addr is smaller than maximum contract size
+                                    TYPE(0),  // is_write
+                                    TYPE(0),  // hi bytes are 0
+                                    bytes[i]  // offset is smaller than maximum contract
+                                              // size
                                 };
                                 lookup(tmp, "zkevm_rw");
                             } else {
                                 tmp = {
                                     TYPE(rw_op_to_num(rw_operation_type::memory)),
                                     current_state.call_id(1),
-                                    addr1 + i,
-                                    TYPE(0),                                               // storage_key_hi
-                                    TYPE(0),                                               // storage_key_lo
-                                    TYPE(0),                                               // field
+                                    offset + i,
+                                    TYPE(0),  // storage_key_hi
+                                    TYPE(0),  // storage_key_lo
+                                    TYPE(0),  // field
                                     current_state.rw_counter(1) + i + 1,
-                                    TYPE(0),                                               // is_write
-                                    TYPE(0),                                               // hi bytes are 0
-                                    bytes[i]                                               // addr is smaller than maximum contract size
+                                    TYPE(0),  // is_write
+                                    TYPE(0),  // hi bytes are 0
+                                    bytes[i]  // offset is smaller than maximum contract
+                                              // size
                                 };
                                 lookup(tmp, "zkevm_rw");
                             }
@@ -181,13 +167,13 @@ namespace nil {
                             TYPE(rw_op_to_num(rw_operation_type::stack)),
                             current_state.call_id(1),
                             current_state.stack_size(1) - 1,
-                            TYPE(0),                                               // storage_key_hi
-                            TYPE(0),                                               // storage_key_lo
-                            TYPE(0),                                               // field
-                            current_state.rw_counter(1)+33,
-                            TYPE(1),                                               // is_write
-                            V_128.first,                                           // hi bytes are 0
-                            V_128.second                                           // addr is smaller than maximum contract size
+                            TYPE(0),  // storage_key_hi
+                            TYPE(0),  // storage_key_lo
+                            TYPE(0),  // field
+                            current_state.rw_counter(1) + 33,
+                            TYPE(1),      // is_write
+                            V_128.first,  // hi bytes are 0
+                            V_128.second  // offset is smaller than maximum contract size
                         };
                         lookup(tmp, "zkevm_rw");
                     } else {
@@ -198,23 +184,25 @@ namespace nil {
 
             template<typename FieldType>
             class zkevm_mload_operation : public opcode_abstract<FieldType> {
-           public:
+              public:
                 virtual void fill_context(
-                    typename generic_component<FieldType, GenerationStage::ASSIGNMENT>::context_type &context,
-                    const opcode_input_type<FieldType, GenerationStage::ASSIGNMENT> &current_state
-                ) override  {
-                    zkevm_mload_bbf<FieldType, GenerationStage::ASSIGNMENT> bbf_obj(context, current_state);
+                    typename generic_component<
+                        FieldType, GenerationStage::ASSIGNMENT>::context_type &context,
+                    const opcode_input_type<FieldType, GenerationStage::ASSIGNMENT>
+                        &current_state) override {
+                    zkevm_mload_bbf<FieldType, GenerationStage::ASSIGNMENT> bbf_obj(
+                        context, current_state);
                 }
                 virtual void fill_context(
-                    typename generic_component<FieldType, GenerationStage::CONSTRAINTS>::context_type &context,
-                    const opcode_input_type<FieldType, GenerationStage::CONSTRAINTS> &current_state
-                )  override {
-                    zkevm_mload_bbf<FieldType, GenerationStage::CONSTRAINTS> bbf_obj(context, current_state);
+                    typename generic_component<
+                        FieldType, GenerationStage::CONSTRAINTS>::context_type &context,
+                    const opcode_input_type<FieldType, GenerationStage::CONSTRAINTS>
+                        &current_state) override {
+                    zkevm_mload_bbf<FieldType, GenerationStage::CONSTRAINTS> bbf_obj(
+                        context, current_state);
                 }
-                virtual  std::size_t rows_amount() override {
-                    return 2;
-                }
+                virtual std::size_t rows_amount() override { return 2; }
             };
-        } // namespace bbf
-    }   // namespace blueprint
-}   // namespace nil
+        }  // namespace bbf
+    }  // namespace blueprint
+}  // namespace nil
