5ec4a56ccb91a3e31be25329e248b7f8cab192c1
Antoine Cyr <acyrntoine@gmail.com>
2025-03-11 15:12:21 -0400

    bytecode circuit with metadata

--- crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/bytecode.hpp
+++ crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/bytecode.hpp
@@ -98,6 +98,7 @@ namespace nil {
                     std::vector<TYPE> push_size = std::vector<TYPE>(max_bytecode_size);
                     std::vector<TYPE> length_left = std::vector<TYPE>(max_bytecode_size);
                     std::vector<TYPE> value_rlc = std::vector<TYPE>(max_bytecode_size);
+                    std::vector<TYPE> is_byte = std::vector<TYPE>(max_bytecode_size);
 
                     if constexpr (stage == GenerationStage::ASSIGNMENT) {
                         std::size_t cur = 0;
@@ -106,26 +107,27 @@ namespace nil {
                             TYPE push_size_value = 0;
                             auto buffer = bytecodes[i].first;
                             TYPE length_left_value = buffer.size();
+                            // HEADER
+                            rlc_challenge[cur] = input.rlc_challenge;
+                            push_size[cur] = 0;
+                            length_left[cur] = length_left_value;
+                            value_rlc[cur] = length_left_value;
+                            is_byte[cur] = tag[cur] * (3 - tag[cur]) * TYPE(2).inversed();
+                            push_size_value = 0;
+                            length_left_value--;
+                            cur++;
+                            // BYTES
                             for(std::size_t j = 0; j < bytecodes[i].first.size(); j++, cur++){
                                 auto byte = buffer[j];
-                                rlc_challenge[cur] = input.rlc_challenge;
-                                if( j == 0){ // HEADER
-                                    push_size[cur] = 0;
-                                    length_left[cur] = length_left_value;
-                                    value_rlc[cur] = length_left_value;
-                                    push_size_value = 0;
-                                    length_left_value--;
-                                    cur++;
-                                }
-                                // BYTE
-                                rlc_challenge[cur] = input.rlc_challenge;
-                                length_left[cur] = length_left_value;
                                 if(push_size_value == 0){
                                     if(byte > 0x5f && byte < 0x80) push_size_value = byte - 0x5f;
                                 } else {
                                     push_size_value--;
                                 }
+                                is_byte[cur] = tag[cur] * (3 - tag[cur]) * TYPE(2).inversed();
                                 push_size[cur] = push_size_value;
+                                rlc_challenge[cur] = input.rlc_challenge;
+                                length_left[cur] = length_left_value;
                                 value_rlc[cur] = value_rlc[cur - 1] * input.rlc_challenge + byte;
                                 length_left_value--;
                             }
@@ -137,44 +139,48 @@ namespace nil {
                         allocate(value_rlc[i],7,i);
                         allocate(length_left[i],8,i);
                         allocate(rlc_challenge[i],9,i);
+                        allocate(is_byte[i],10,i);
                     }
                     // constrain all bytecode values
 //                    if (make_links) {
 //                        copy_constrain(input.rlc_challenge, rlc_challenge[0]);
 //                    }
                     static const auto zerohash = zkevm_keccak_hash({});
-                    if constexpr (stage == GenerationStage::CONSTRAINTS) {
-                        std::vector<TYPE> every;
-                        std::vector<TYPE> non_first;
-                        std::vector<TYPE> bytes;
+                    for(std::size_t i = 0; i < max_bytecode_size; i++) {
+                        constrain(tag[i] * (tag[i] - 1) * (tag[i] - 2));    // 0. TAG is zeroes, one or two
+                        constrain((tag[i] - 2) * (tag[i] - 1) * index[i]);     // 1. INDEX for HEADER and unused bytes is zero
+                        constrain((tag[i] - 2) * (tag[i] - 1) * (length_left[i] - value[i])); // 4. In contract header length_left == contract length
+                        constrain(is_opcode[i] * (is_opcode[i] - 1)); // 7. is_opcode is zeroes or ones
+                        constrain((tag[i] - 2) * (tag[i] - 1) * is_opcode[i]); // 8. is_opcode on HEADER are zeroes
+                        constrain((tag[i] - 2) * (tag[i] - 1) * (value_rlc[i] - length_left[i])); // 14. value_rlc for HEADERS == 0;       
 
-                        // Every constraints
-                        every.push_back(context_object.relativize(tag[0] * (tag[0] - 1), -1));                // 0. TAG is zeroes or ones -- maybe there will be third value for non-used rows
-                        every.push_back(context_object.relativize((tag[0] - 1) * index[0], -1));              // 1. INDEX for HEADER and unused bytes is zero
-                        every.push_back(context_object.relativize((tag[0] - 1) * (length_left[0] - value[0]), -1)); // 4. In contract header length_left == contract length
-                        every.push_back(context_object.relativize(is_opcode[0] * (is_opcode[0] - 1), -1));    // 7. is_opcode is zeroes or ones
-                        every.push_back(context_object.relativize((tag[0] - 1) * is_opcode[0], -1));          // 8. is_opcode on HEADER are zeroes
-                        every.push_back(context_object.relativize((tag[0] - 1) * (value_rlc[0] - length_left[0]), -1)); // 14. value_rlc for HEADERS == 0;
+                        if (i > 0) {
+                            constrain((tag[i-1] - 2) * (tag[i-1] - 1) * index[i]); // 2. INDEX for first contract byte is zero
+                            constrain(tag[i-1] * tag[i] * (index[i] - index[i-1] - 1)); // 3. INDEX is incremented for all bytes
+                            constrain(tag[i] * (length_left[i-1] - length_left[i] - 1)); // 5. In contract bytes each row decrement length_left
+                            constrain(tag[i-1] * (tag[i] - 2) * (tag[i] - 1) * length_left[i-1]); // 6. Length_left is zero for last byte in the contract
+                            constrain((tag[i] - 2) * (tag[i-1] - 1) * tag[i] * (is_opcode[i] - 1)); // 9. Fist is_opcode on BYTE after HEADER is 1
+                            constrain((tag[i] - 2) * tag[i] * (is_opcode[i] - 1) * (push_size[i-1] - push_size[i] - 1)); // 10. PUSH_SIZE decreases for non-opcodes except metadata
+                            constrain(is_opcode[i] * push_size[i-1]); // 11. before opcode push_size is always zero
+                            constrain(tag[i] * (hash_hi[i-1] - hash_hi[i])); //12. for all bytes hash is similar to previous
+                            constrain(tag[i] * (hash_lo[i-1] - hash_lo[i])); //13. for all bytes hash is similar to previous
+                            constrain(tag[i] * (value_rlc[i] - value_rlc[i-1] * rlc_challenge[i] - value[i])); // 15. for all bytes RLC is correct
+                            constrain(tag[i] * (rlc_challenge[i] - rlc_challenge[i-1])); //16. for each BYTEs rlc_challenge are similar
+                        }
+                        if (i> 0 && i < max_bytecode_size-1) {
+                            constrain(tag[i+1] * (rlc_challenge[i] - rlc_challenge[i-1])); //17. rlc_challenge is similar for different contracts
+                        }
+                        lookup(tag[i]*value[i]*(2-tag[i]),"byte_range_table/full");
+                        lookup(std::vector<TYPE>({value[i]*is_opcode[i], push_size[i]*is_opcode[i], is_opcode[i]}),"zkevm_opcodes/full");
 
-                        // Non-first row constraints
-                        non_first.push_back(context_object.relativize((tag[0] - 1) * index[1], -1));                  // 2. INDEX for first contract byte is zero
-                        non_first.push_back(context_object.relativize(tag[0] * tag[1] * (index[1] - index[0] - 1), -1)); // 3. INDEX is incremented for all bytes
-                        non_first.push_back(context_object.relativize(tag[1] * (length_left[0] - length_left[1] - 1), -1)); // 5. In contract bytes each row decrement length_left
-                        non_first.push_back(context_object.relativize(tag[0] * (tag[1] - 1) * length_left[0], -1));     // 6. Length_left is zero for last byte in the contract
-                        non_first.push_back(context_object.relativize((tag[0] - 1) * tag[1] * (is_opcode[1] - 1), -1));   // 9. Fist is_opcode on BYTE after HEADER is 1
-                        non_first.push_back(context_object.relativize(tag[1] * (is_opcode[1] - 1) * (push_size[0] - push_size[1] - 1), -1)); // 10. PUSH_SIZE decreases for non-opcodes
-                        non_first.push_back(context_object.relativize(is_opcode[1] * push_size[0], -1));                  // 11. before opcode push_size is always zero
-                        non_first.push_back(context_object.relativize(tag[1] * (hash_hi[0] - hash_hi[1]), -1));           // 12. for all bytes hash is similar to previous
-                        non_first.push_back(context_object.relativize(tag[1] * (hash_lo[0] - hash_lo[1]), -1));           // 13. for all bytes hash is similar to previous
-                        non_first.push_back(context_object.relativize(tag[1] * (value_rlc[1] - value_rlc[0] * rlc_challenge[1] - value[1]), -1)); // 15. for all bytes RLC is correct
-                        non_first.push_back(context_object.relativize(tag[1] * (rlc_challenge[1] - rlc_challenge[0]), -1)); // 16. for each BYTEs rlc_challenge are similar
-
-                        // Bytes constraint
-                        bytes.push_back(context_object.relativize(tag[2] * (rlc_challenge[1] - rlc_challenge[0]), -1)); // 17. rlc_challenge is similar for different contracts
-
-                        // Apply constraints
-                        for (std::size_t i = 0; i < every.size(); i++) {
-                            context_object.relative_constrain(every[i], 0, max_bytecode_size - 1);
+                        if( i > 0 ){
+                            //is last
+                            lookup(std::vector<TYPE>({
+                                tag[i] + 1 - tag[i], // TODO: update math::expression constructor with constant parameter
+                                is_byte[i-1] * (1 - is_byte[i])  * value_rlc[i-1],
+                                is_byte[i-1] * (1 - is_byte[i]) * hash_hi[i-1] + (1 - is_byte[i-1] * (1 - is_byte[i])) * w_hi<FieldType>(zerohash),
+                                is_byte[i-1] * (1 - is_byte[i]) * hash_lo[i-1] + (1 - is_byte[i-1] * (1 - is_byte[i])) * w_lo<FieldType>(zerohash)
+                            }), "keccak_table");
                         }
                         for (std::size_t i = 0; i < non_first.size(); i++) {
                             context_object.relative_constrain(non_first[i], 1, max_bytecode_size - 1);
--- crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/subcomponents/bytecode_table.hpp
+++ crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/subcomponents/bytecode_table.hpp
@@ -1,5 +1,6 @@
 //---------------------------------------------------------------------------//
 // Copyright (c) 2024 Elena Tatuzova <e.tatuzova@nil.foundation>
+// Copyright (c) 2025 Antoine Cyr <antoinecyr@nil.foundation>
 //
 // MIT License
 //
@@ -30,8 +31,6 @@
 namespace nil {
     namespace blueprint {
         namespace bbf {
-            // Component for bytecode table
-
             template<typename FieldType, GenerationStage stage>
             class bytecode_table : public generic_component<FieldType, stage> {
                 using typename generic_component<FieldType, stage>::context_type;
@@ -41,86 +40,104 @@ namespace nil {
                 using generic_component<FieldType, stage>::lookup;
                 using generic_component<FieldType, stage>::lookup_table;
 
-                public:
-                using typename generic_component<FieldType,stage>::TYPE;
+            public:
+                using TYPE = typename generic_component<FieldType, stage>::TYPE;
                 using input_type = std::conditional_t<
                     stage == GenerationStage::ASSIGNMENT, zkevm_keccak_buffers, std::monostate
                 >;
 
                 std::size_t max_bytecode_size;
 
-                // interfaces for interaction with other components:
-                std::vector<TYPE> tag = std::vector<TYPE>(max_bytecode_size);
-                std::vector<TYPE> index = std::vector<TYPE>(max_bytecode_size);
-                std::vector<TYPE> value = std::vector<TYPE>(max_bytecode_size);
-                std::vector<TYPE> is_opcode = std::vector<TYPE>(max_bytecode_size);
-                std::vector<TYPE> hash_hi = std::vector<TYPE>(max_bytecode_size);
-                std::vector<TYPE> hash_lo = std::vector<TYPE>(max_bytecode_size);
+                std::vector<TYPE> tag = std::vector<TYPE>(max_bytecode_size);       // 0: header, 1: executable bytes, 2: metadata
+                std::vector<TYPE> index = std::vector<TYPE>(max_bytecode_size);     // Position in bytecode
+                std::vector<TYPE> value = std::vector<TYPE>(max_bytecode_size);     // Byte value or total length
+                std::vector<TYPE> is_opcode = std::vector<TYPE>(max_bytecode_size); // 1 if opcode, 0 otherwise
+                std::vector<TYPE> hash_hi = std::vector<TYPE>(max_bytecode_size);   // High 128 bits of hash
+                std::vector<TYPE> hash_lo = std::vector<TYPE>(max_bytecode_size);   // Low 128 bits of hash
 
-                static std::size_t get_witness_amount(){
-                    return 6;
-                }
+                static std::size_t get_witness_amount() { return 6; }
 
                 bytecode_table(context_type &context_object,
-                                    const input_type &input,
-                                    std::size_t max_bytecode_size_,
-                                    bool make_links = true) :
-                    max_bytecode_size(max_bytecode_size_),
-                    generic_component<FieldType,stage>(context_object) {
+                               const input_type &input,
+                               std::size_t max_bytecode_size_,
+                               bool make_links = true)
+                    : max_bytecode_size(max_bytecode_size_),
+                      generic_component<FieldType, stage>(context_object) {
 
-                    // if we're in assignment stage, prepare all the values
                     if constexpr (stage == GenerationStage::ASSIGNMENT) {
                         auto bytecodes = input.get_data();
-
                         std::size_t cur = 0;
-                        for(std::size_t i = 0; i < bytecodes.size(); i++) {
+
+                        for (std::size_t i = 0; i < bytecodes.size(); i++) {
                             TYPE hash_hi_val = w_hi<FieldType>(bytecodes[i].second);
                             TYPE hash_lo_val = w_lo<FieldType>(bytecodes[i].second);
                             TYPE push_size = 0;
                             const auto &buffer = bytecodes[i].first;
-                            for(std::size_t j = 0; j < buffer.size(); j++, cur++){
+                            std::size_t total_len = buffer.size();
+
+                            // Compute metadata length from the last two bytes
+                            std::size_t exec_boundary = total_len; // Default: all bytes executable
+                            if (total_len >= 2) {
+                                std::size_t meta_len = (buffer[total_len - 2] << 8) + buffer[total_len - 1];
+                                if (meta_len + 2 <= total_len) {
+                                    std::size_t boundary = total_len - meta_len - 2 - 1; // Byte before metadata
+                                    if (boundary < total_len && (buffer[boundary] == 0x00 || buffer[boundary] == 0xfe || buffer[boundary] == 0xf3)) {
+                                        exec_boundary = boundary + 1; // After stopping opcode
+                                    }
+                                }
+                            }
+
+                            // Header
+                            BOOST_ASSERT(cur < max_bytecode_size);
+                            tag[cur] = 0;
+                            index[cur] = 0;
+                            value[cur] = total_len;
+                            is_opcode[cur] = 0;
+                            hash_hi[cur] = hash_hi_val;
+                            hash_lo[cur] = hash_lo_val;
+                            cur++;
+
+                            // Bytes
+                            for (std::size_t j = 0; j < buffer.size(); j++, cur++) {
                                 BOOST_ASSERT(cur < max_bytecode_size);
                                 std::uint8_t byte = buffer[j];
-                                hash_hi[cur] = hash_hi_val;
-                                hash_lo[cur] = hash_lo_val;
-                                if( j == 0) { // HEADER
-                                    value[cur] = buffer.size();
-                                    tag[cur] = 0;
-                                    index[cur] = 0;
-                                    is_opcode[cur] = 0;
-                                    push_size = 0; // might be unnecessary
-                                    cur++;
-                                }
-                                // BYTE
                                 value[cur] = byte;
                                 hash_hi[cur] = hash_hi_val;
                                 hash_lo[cur] = hash_lo_val;
-                                tag[cur] = 1;
                                 index[cur] = j;
-                                if (push_size == 0) {
-                                    is_opcode[cur] = 1;
-                                    if (byte > 0x5f && byte < 0x80) push_size = byte - 0x5f;
-                                } else {
+
+                                if (j < exec_boundary) { // Executable bytes
+                                    tag[cur] = 1;
+                                    if (push_size == 0) {
+                                        is_opcode[cur] = 1;
+                                        if (byte > 0x5f && byte < 0x80) {
+                                            push_size = byte - 0x5f;
+                                        }
+                                    } else {
+                                        is_opcode[cur] = 0;
+                                        push_size--;
+                                    }
+                                } else { // Metadata bytes
+                                    tag[cur] = 2;
                                     is_opcode[cur] = 0;
-                                    push_size--;
+                                    push_size = 0; // Reset to avoid carry-over
                                 }
                                 //std::cout << cur << ". " << std::hex << std::size_t(byte) << " " << is_opcode[cur] << " " << push_size << std::dec << std::endl;
                             }
                         }
                     }
-                    // allocate everything. NB: this replaces the map from the original component
-                    for(std::size_t i = 0; i < max_bytecode_size; i++) {
-                        allocate(tag[i],0,i);
-                        allocate(index[i],1,i);
-                        allocate(value[i],2,i);
-                        allocate(is_opcode[i],3,i);
-                        allocate(hash_hi[i],4,i);
-                        allocate(hash_lo[i],5,i);
+
+                    for (std::size_t i = 0; i < max_bytecode_size; i++) {
+                        allocate(tag[i], 0, i);
+                        allocate(index[i], 1, i);
+                        allocate(value[i], 2, i);
+                        allocate(is_opcode[i], 3, i);
+                        allocate(hash_hi[i], 4, i);
+                        allocate(hash_lo[i], 5, i);
                     }
-                    // declare dynamic lookup table
-                    lookup_table("zkevm_bytecode",std::vector<std::size_t>({0,1,2,3,4,5}),0,max_bytecode_size);
-                };
-	    };
+                    lookup_table("zkevm_bytecode", std::vector<std::size_t>({0, 1, 2, 3, 4, 5}), 0, max_bytecode_size);
+                }
+            };
         }
     }
 }
