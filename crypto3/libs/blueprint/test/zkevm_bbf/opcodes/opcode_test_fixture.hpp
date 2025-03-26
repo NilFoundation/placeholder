974ba8dca6fb92702ff478728b2a4f543676d37c
Andrey Nefedov <ioxid@nil.foundation>
2025-03-26 23:14:05 +0000

    revert parts with benchmarking small fields on small rw circuit using logx

--- crypto3/libs/blueprint/test/zkevm_bbf/opcodes/opcode_test_fixture.hpp
+++ crypto3/libs/blueprint/test/zkevm_bbf/opcodes/opcode_test_fixture.hpp
@@ -24,14 +24,9 @@
 
 #pragma once
 
-#include <boost/test/test_tools.hpp>
-
 #include <nil/crypto3/algebra/curves/pallas.hpp>
-#include <nil/crypto3/algebra/curves/vesta.hpp>
-#include <nil/crypto3/algebra/fields/arithmetic_params/alt_bn128.hpp>
-#include <nil/crypto3/algebra/fields/arithmetic_params/babybear.hpp>
-#include <nil/crypto3/algebra/fields/arithmetic_params/goldilocks.hpp>
 #include <nil/crypto3/algebra/fields/arithmetic_params/pallas.hpp>
+#include <nil/crypto3/algebra/curves/vesta.hpp>
 #include <nil/crypto3/algebra/fields/arithmetic_params/vesta.hpp>
 #include <nil/crypto3/algebra/random_element.hpp>
 
@@ -57,15 +52,9 @@
 #include <nil/blueprint/zkevm_bbf/exp.hpp>
 #include <nil/blueprint/zkevm_bbf/keccak.hpp>
 #include <nil/blueprint/zkevm_bbf/rw.hpp>
-#include <nil/blueprint/zkevm_bbf/rw_small_field.hpp>
 #include <nil/blueprint/zkevm_bbf/zkevm.hpp>
 
 #include "../circuit_test_fixture.hpp"
-#include "nil/blueprint/bbf/enums.hpp"
-
-#include <nil/crypto3/algebra/fields/babybear.hpp>
-#include <nil/crypto3/algebra/fields/goldilocks.hpp>
-#include <nil/crypto3/algebra/fields/pallas/base_field.hpp>
 
 using namespace nil::crypto3;
 using namespace nil::blueprint;
@@ -85,36 +74,32 @@ public:
 
         integral_type base16 = integral_type(1) << 16;
 
-        // std::size_t max_keccak_blocks = max_sizes.max_keccak_blocks;
-        // std::size_t max_bytecode = max_sizes.max_bytecode;
+        std::size_t max_keccak_blocks = max_sizes.max_keccak_blocks;
+        std::size_t max_bytecode = max_sizes.max_bytecode;
         std::size_t max_mpt = max_sizes.max_mpt;
         std::size_t max_rw = max_sizes.max_rw;
-        // std::size_t max_copy = max_sizes.max_copy;
-        // std::size_t max_zkevm_rows = max_sizes.max_zkevm_rows;
-        // std::size_t max_exponentiations = max_sizes.max_exponentiations;
-        // std::size_t max_exp_rows = max_sizes.max_exp_rows;
+        std::size_t max_copy = max_sizes.max_copy;
+        std::size_t max_zkevm_rows = max_sizes.max_zkevm_rows;
+        std::size_t max_exponentiations = max_sizes.max_exponentiations;
+        std::size_t max_exp_rows = max_sizes.max_exp_rows;
         std::size_t max_call_commits = max_sizes.max_call_commits;
 
-        // typename bbf::copy<BlueprintFieldType, GenerationStage::ASSIGNMENT>::input_type copy_assignment_input;
-        // copy_assignment_input.rlc_challenge = 7;
-        // copy_assignment_input.bytecodes = circuit_inputs.bytecodes();
-        // copy_assignment_input.keccak_buffers = circuit_inputs.keccaks();
+        typename bbf::copy<BlueprintFieldType, GenerationStage::ASSIGNMENT>::input_type copy_assignment_input;
+        copy_assignment_input.rlc_challenge = 7;
+        copy_assignment_input.bytecodes = circuit_inputs.bytecodes();
+        copy_assignment_input.keccak_buffers = circuit_inputs.keccaks();
         copy_assignment_input.rw_operations = circuit_inputs.rw_operations();
-        // copy_assignment_input.copy_events = circuit_inputs.copy_events();
-        // copy_assignment_input.call_commits = circuit_inputs.call_commits();
+        copy_assignment_input.copy_events = circuit_inputs.copy_events();
+        copy_assignment_input.call_commits = circuit_inputs.call_commits();
 
-        // typename zkevm<BlueprintFieldType, GenerationStage::ASSIGNMENT>::input_type
-        // zkevm_assignment_input; zkevm_assignment_input.rlc_challenge = 7;
-        // zkevm_assignment_input.bytecodes = circuit_inputs.bytecodes();
-        // zkevm_assignment_input.keccak_buffers = circuit_inputs.keccaks();
-        // zkevm_assignment_input.rw_operations = circuit_inputs.rw_operations();
-
-        // std::cerr << "zkevm_assignment_input.rw_operations.size(): "
-        //           << zkevm_assignment_input.rw_operations.size() << std::endl;
-
-        // zkevm_assignment_input.copy_events = circuit_inputs.copy_events();
-        // zkevm_assignment_input.zkevm_states = circuit_inputs.zkevm_states();
-        // zkevm_assignment_input.exponentiations = circuit_inputs.exponentiations();
+        typename zkevm<BlueprintFieldType, GenerationStage::ASSIGNMENT>::input_type zkevm_assignment_input;
+        zkevm_assignment_input.rlc_challenge = 7;
+        zkevm_assignment_input.bytecodes = circuit_inputs.bytecodes();
+        zkevm_assignment_input.keccak_buffers = circuit_inputs.keccaks();
+        zkevm_assignment_input.rw_operations = circuit_inputs.rw_operations();
+        zkevm_assignment_input.copy_events = circuit_inputs.copy_events();
+        zkevm_assignment_input.zkevm_states = circuit_inputs.zkevm_states();
+        zkevm_assignment_input.exponentiations = circuit_inputs.exponentiations();
 
         typename rw<BlueprintFieldType, GenerationStage::ASSIGNMENT>::input_type rw_assignment_input;
         rw_assignment_input.rw_operations = circuit_inputs.rw_operations();
@@ -124,158 +109,83 @@ public:
         call_commit_assignment_input.rw_operations = circuit_inputs.rw_operations();
         call_commit_assignment_input.call_commits = circuit_inputs.call_commits();
 
-        // typename
-        // nil::blueprint::bbf::zkevm_keccak<BlueprintFieldType,nil::blueprint::bbf::GenerationStage::ASSIGNMENT>::input_type
-        // keccak_assignment_input; keccak_assignment_input.rlc_challenge = 7;
-        // keccak_assignment_input.private_input = circuit_inputs.keccaks();
+        typename nil::blueprint::bbf::zkevm_keccak<BlueprintFieldType,nil::blueprint::bbf::GenerationStage::ASSIGNMENT>::input_type keccak_assignment_input;
+        keccak_assignment_input.rlc_challenge = 7;
+        keccak_assignment_input.private_input = circuit_inputs.keccaks();
 
-        // typename bytecode<BlueprintFieldType, GenerationStage::ASSIGNMENT>::input_type
-        // bytecode_assignment_input; bytecode_assignment_input.rlc_challenge = 7;
-        // bytecode_assignment_input.bytecodes = circuit_inputs.bytecodes();
-        // bytecode_assignment_input.keccak_buffers = circuit_inputs.keccaks();
+        typename bytecode<BlueprintFieldType, GenerationStage::ASSIGNMENT>::input_type bytecode_assignment_input;
+        bytecode_assignment_input.rlc_challenge = 7;
+        bytecode_assignment_input.bytecodes = circuit_inputs.bytecodes();
+        bytecode_assignment_input.keccak_buffers = circuit_inputs.keccaks();
 
-        // auto exp_assignment_input = circuit_inputs.exponentiations();
+
+        auto exp_assignment_input = circuit_inputs.exponentiations();
 
         bool result;
 
-        // // Max_rows, max_bytecode, max_rw
-        // result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::zkevm>(
-        //     "zkevm",
-        //     {}, zkevm_assignment_input,
-        //     max_zkevm_rows,
-        //     max_copy,
-        //     max_rw,
-        //     max_exponentiations,
-        //     max_bytecode
-        // );
-        // BOOST_CHECK(result);
-        // std::cout << std::endl;
+        // Max_rows, max_bytecode, max_rw
+        result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::zkevm>(
+            "zkevm",
+            {}, zkevm_assignment_input,
+            max_zkevm_rows,
+            max_copy,
+            max_rw,
+            max_exponentiations,
+            max_bytecode
+        );
+        BOOST_CHECK(result);
+        std::cout << std::endl;
 
-        // std::cout << "Exp circuit" << std::endl;
-        // result = test_bbf_component<BlueprintFieldType,
-        // nil::blueprint::bbf::exponentiation>(
-        //     "exp",
-        //     {}, exp_assignment_input,
-        //     max_exp_rows,
-        //     max_exponentiations
-        // );
-        // BOOST_CHECK(result);
-        // std::cout << std::endl;
+        std::cout << "Exp circuit" << std::endl;
+        result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::exponentiation>(
+            "exp",
+            {}, exp_assignment_input,
+            max_exp_rows,
+            max_exponentiations
+        );
+        BOOST_CHECK(result);
+        std::cout << std::endl;
 
-        // // Max_bytecode, max_bytecode
-        // std::cout << "Bytecode circuit" << std::endl;
-        // result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::bytecode>(
-        //     "bytecode",
-        //     {7}, bytecode_assignment_input, max_bytecode, max_keccak_blocks
-        // );
-        // BOOST_CHECK(result);
-        // std::cout << std::endl;
+        // Max_bytecode, max_bytecode
+        std::cout << "Bytecode circuit" << std::endl;
+        result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::bytecode>(
+            "bytecode",
+            {7}, bytecode_assignment_input, max_bytecode, max_keccak_blocks
+        );
+        BOOST_CHECK(result);
+        std::cout << std::endl;
 
-        // // Max_rw, Max_mpt
-        // std::cout << "RW circuit" << std::endl;
-        // result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::rw>(
-        //     "rw",
-        //     {}, rw_assignment_input, max_rw, max_mpt, max_call_commits
-        // );
-        // BOOST_CHECK(result);
-        // std::cout << std::endl;
+        // Max_rw, Max_mpt
+        std::cout << "RW circuit" << std::endl;
+        result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::rw>(
+            "rw",
+            {}, rw_assignment_input, max_rw, max_mpt, max_call_commits
+        );
+        BOOST_CHECK(result);
+        std::cout << std::endl;
 
-        auto test_field =
-            [&]<typename FieldT, template<typename, GenerationStage> class ComponentT>(
-                std::string name) {
-                std::cout << std::endl;
-                std::cout << "=========== RW circuit " << name
-                          << " ===========" << std::endl;
-                std::cout << std::endl;
-                result = test_bbf_component<FieldT, ComponentT>(
-                    "rw", {}, rw_assignment_input, max_rw, max_mpt, max_call_commits);
-                BOOST_CHECK(result);
-                std::cout << std::endl;
-                std::cout << "=========== RW circuit " << name
-                          << ": end ===========" << std::endl;
-                std::cout << std::endl;
-            };
+        // TODO: enable when call commit circuit supports empty write lists
+        // std::cout << "Call commits circuit" << std::endl;
+        // // Max_rw, Max_call_commits
+        // result = test_bbf_component<BlueprintFieldType, call_commit>(
+        //     "call_commit", {}, call_commit_assignment_input,
+        //     max_rw, max_call_commits);
 
-        test_field.template
-        operator()<nil::crypto3::algebra::fields::alt_bn128_scalar_field<254u>,
-                   nil::blueprint::bbf::rw>("bn254");
+        // Max_copy, Max_rw, Max_keccak, Max_bytecode
+        result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::copy>(
+            "copy",
+            {7}, copy_assignment_input,
+            max_copy, max_rw, max_keccak_blocks, max_bytecode, max_call_commits
+        );
+        BOOST_CHECK(result);
+        std::cout << std::endl;
 
-        test_field.template
-        operator()<nil::crypto3::algebra::fields::alt_bn128_scalar_field<254u>,
-                   nil::blueprint::bbf::rw_small_field>("bn254_on_small");
-
-        test_field.template operator()<nil::crypto3::algebra::fields::goldilocks,
-                                       nil::blueprint::bbf::rw_small_field>("goldilocks");
-
-        // test_field.template operator()<
-        //     nil::crypto3::algebra::fields::fp2<nil::crypto3::algebra::fields::goldilocks>,
-        //     nil::blueprint::bbf::rw_small_field>("goldilocks_fp2");
-
-        test_field.template
-        operator()<nil::crypto3::algebra::fields::babybear_simple_31_bit,
-                   nil::blueprint::bbf::rw_small_field>("babybear_simple_31_bit");
-
-        test_field.template operator()<nil::crypto3::algebra::fields::babybear,
-                                       nil::blueprint::bbf::rw_small_field>("babybear");
-
-        // // Max_rw, Max_mpt
-        // std::cerr << "RW circuit bn254" << std::endl;
-        // result = test_bbf_component<
-        //     nil::crypto3::algebra::fields::alt_bn128_scalar_field<254u>,
-        //     nil::blueprint::bbf::rw>("rw", {}, rw_assignment_input, max_rw, max_mpt);
-        // BOOST_CHECK(result);
-        // std::cerr << "RW circuit bn254: end" << std::endl;
-
-        // // Max_rw, Max_mpt
-        // std::cerr << "RW circuit babybear_montgomery_big_mod field" << std::endl;
-        // result =
-        // test_bbf_component<nil::crypto3::algebra::fields::babybear_montgomery_big_mod,
-        //                             nil::blueprint::bbf::rw_small_field>(
-        //     "rw", {}, rw_assignment_input, max_rw, max_mpt);
-        // BOOST_CHECK(result);
-        // std::cerr << "RW circuit babybear_montgomery_big_mod field: end" << std::endl;
-
-        // // Max_rw, Max_mpt
-        // std::cerr << "RW circuit goldilocks field" << std::endl;
-        // result = test_bbf_component<nil::crypto3::algebra::fields::goldilocks,
-        //                             nil::blueprint::bbf::rw_small_field>(
-        //     "rw", {}, rw_assignment_input, max_rw, max_mpt);
-        // BOOST_CHECK(result);
-        // std::cerr << "RW circuit goldilocks field: end" << std::endl;
-
-        // // Max_rw, Max_mpt
-        // std::cerr << "RW circuit babybear_simple_31_bit field" << std::endl;
-        // result =
-        // test_bbf_component<nil::crypto3::algebra::fields::babybear_simple_31_bit,
-        //                             nil::blueprint::bbf::rw_small_field>(
-        //     "rw", {}, rw_assignment_input, max_rw, max_mpt);
-        // BOOST_CHECK(result);
-        // std::cerr << "RW circuit babybear_simple_31_bit field: end" << std::endl;
-
-        // // Max_rw, Max_mpt
-        // std::cerr << "RW circuit babybear field" << std::endl;
-        // result = test_bbf_component<nil::crypto3::algebra::fields::babybear,
-        //                             nil::blueprint::bbf::rw_small_field>(
-        //     "rw", {}, rw_assignment_input, max_rw, max_mpt);
-        // BOOST_CHECK(result);
-        // std::cerr << "RW circuit babybear field: end" << std::endl;
-
-        // // Max_copy, Max_rw, Max_keccak, Max_bytecode
-        // result =test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::copy>(
-        //     "copy",
-        //     {7}, copy_assignment_input,
-        //     max_copy, max_rw, max_keccak_blocks, max_bytecode
-        // );
-        // BOOST_CHECK(result);
-        // std::cout << std::endl;
-
-        // // Max_keccak
-        // result = test_bbf_component<BlueprintFieldType,
-        // nil::blueprint::bbf::zkevm_keccak>(
-        //     "keccak",
-        //     {}, keccak_assignment_input , max_keccak_blocks
-        // );
-        // BOOST_CHECK(result);
-        // std::cout << std::endl;
+        // Max_keccak
+        result = test_bbf_component<BlueprintFieldType, nil::blueprint::bbf::zkevm_keccak>(
+            "keccak",
+            {}, keccak_assignment_input , max_keccak_blocks
+        );
+        BOOST_CHECK(result);
+        std::cout << std::endl;
     }
 };
