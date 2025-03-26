743462129e658dcea16f7958fbaea01fe3a71f44
Andrey Nefedov <ioxid@nil.foundation>
2025-03-26 10:01:20 +0000

    fix single-thread compilation on clang

--- crypto3/libs/zk/include/nil/crypto3/zk/snark/systems/plonk/placeholder/lookup_argument.hpp
+++ crypto3/libs/zk/include/nil/crypto3/zk/snark/systems/plonk/placeholder/lookup_argument.hpp
@@ -364,8 +364,8 @@ namespace nil {
                             variable_values_out[var] = assignment;
                         }
 
-                        bench::scoped_log(
-                            std::format("Variables count: {}", variable_values_outs.size()));
+                        bench::scoped_log(std::format("Variables count: {}",
+                                                      variable_values_out.size()));
                     }
 
                     // Run over all the lookup inputs, and collect a vector of expressions that will need to be evaluated.
