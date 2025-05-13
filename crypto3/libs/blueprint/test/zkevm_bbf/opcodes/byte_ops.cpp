4d299383ed8d01ccec323bcc8a492be97d2df19b
Javier Silva <93121149+javiersilva0@users.noreply.github.com>
2025-05-13 11:30:34 +0200

    fixed typo in tests

--- crypto3/libs/blueprint/test/zkevm_bbf/opcodes/byte_ops.cpp
+++ crypto3/libs/blueprint/test/zkevm_bbf/opcodes/byte_ops.cpp
@@ -171,7 +171,9 @@ BOOST_AUTO_TEST_CASE(byte_ops) {
     opcode_tester.push_opcode(
         zkevm_opcode::PUSH32,
         0x8b70726fb8d3a24da9ff9647225a18412b8f010425938504d73ebc8801e2e016_big_uint256);
-    opcode_tester.push_opcode(zkevm_opcode::PUSH32,257);    opcode_tester.push_opcode(
+    opcode_tester.push_opcode(zkevm_opcode::PUSH32,257);    
+    opcode_tester.push_opcode(zkevm_opcode::SHR);
+    opcode_tester.push_opcode(
         zkevm_opcode::PUSH32,
         0x8b70726fb8d3a24da9ff9647225a18412b8f010425938504d73ebc8801e2e016_big_uint256);
     opcode_tester.push_opcode(zkevm_opcode::PUSH32,256);
@@ -333,7 +335,7 @@ BOOST_AUTO_TEST_CASE(byte_ops) {
     max_sizes.max_mpt = 0;
     max_sizes.max_rw = 3000;
     max_sizes.max_copy = 500;
-    max_sizes.max_zkevm_rows = 600;
+    max_sizes.max_zkevm_rows = 700;
     max_sizes.max_exp_rows = 500;
     max_sizes.max_exponentiations = 50;
 
