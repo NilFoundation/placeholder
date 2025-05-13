89d4fbc0c3edf699eb8708d1526437eca9b5458e
Javier Silva <93121149+javiersilva0@users.noreply.github.com>
2025-05-13 18:44:24 +0200

    added copyright

--- crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/opcodes/shl.hpp
+++ crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/opcodes/shl.hpp
@@ -1,6 +1,7 @@
 //---------------------------------------------------------------------------//
 // Copyright (c) 2024 Alexey Yashunsky <a.yashunsky@nil.foundation>
 // Copyright (c) 2024 Antoine Cyr <antoine.cyr@nil.foundation>
+// Copyright (c) 2025 Javier Silva <javier.silva@nil.foundation>
 //
 // MIT License
 //
