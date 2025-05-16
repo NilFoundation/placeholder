b4a2696d876d4e711cdaa536b612d64032d5213a
Javier Silva <93121149+javiersilva0@users.noreply.github.com>
2025-05-16 12:33:46 +0200

    weird allocation bug

--- crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/opcodes/mul.hpp
+++ crypto3/libs/blueprint/include/nil/blueprint/zkevm_bbf/opcodes/mul.hpp
@@ -27,6 +27,7 @@
 #pragma once
 
 #include <alloca.h>
+#include <unistd.h>
 #include <cstddef>
 #include <numeric>
 #include <algorithm>
@@ -59,11 +60,6 @@ class zkevm_mul_bbf : generic_component<FieldType, stage> {
 public:
     using typename generic_component<FieldType,stage>::TYPE;
     using typename generic_component<FieldType, stage>::context_type;
-
-    constexpr static const typename FieldType::value_type two_64 =
-        0x10000000000000000_big_uint256;
-    constexpr static const typename FieldType::value_type two_128 = 0x100000000000000000000000000000000_big_uint254;
-    constexpr static const typename FieldType::value_type two_192 = 0x1000000000000000000000000000000000000000000000000_big_uint254;
 
     // Computes the terms of r*b with coefficient 2^(8 * chunk_index)
     TYPE carryless_mul(const std::vector<TYPE> &a_8_chunks,
@@ -118,123 +114,138 @@ public:
         std::vector<TYPE> r8_chunks_check(chunk_8_amount);
         std::vector<TYPE> r8_carries_check(chunk_8_amount);
 
+        allocate(a8_chunks[0], 0, 5);   // This one crashes the tests:
+                                        // unknown location(0): fatal error: in "zkevm_opcode_test_suite/mul": std::runtime_error: Insufficient space for starting a new row.
+
+        allocate(a8_chunks[0], 1, 5);   // All of these work fine
+        allocate(a8_chunks[0], 0, 4);
+        allocate(a8_chunks[0], 0, 6);
+
 
         // PART 1: computing the opcode and splitting values in chunks
-        if constexpr (stage == GenerationStage:: ASSIGNMENT) {
-            // Extract input values from stack
-            zkevm_word_type b = current_state.stack_top();    
-            zkevm_word_type a = current_state.stack_top(1);   
+        // if constexpr (stage == GenerationStage:: ASSIGNMENT) {
+        //     // Extract input values from stack
+        //     zkevm_word_type b = current_state.stack_top();    
+        //     zkevm_word_type a = current_state.stack_top(1);   
 
-            zkevm_word_type r = wrapping_mul(a, b);      // Result
+        //     zkevm_word_type r = wrapping_mul(a, b);      // Result
 
-            // 16-bit chunks
-            a_chunks = zkevm_word_to_field_element<FieldType>(a);
-            b_chunks = zkevm_word_to_field_element<FieldType>(b);
-            r_chunks = zkevm_word_to_field_element<FieldType>(r);
-            for (std::size_t i = 0; i < chunk_amount; i++) {
-                allocate(a_chunks[i], 2 * chunk_amount + i, 6);
-                allocate(b_chunks[i], 2 * chunk_amount + i, 7);
-                allocate(r_chunks[i], 2 * chunk_amount + i, 8);
-            }
+        //     // 16-bit chunks
+        //     a_chunks = zkevm_word_to_field_element<FieldType>(a);
+        //     b_chunks = zkevm_word_to_field_element<FieldType>(b);
+        //     r_chunks = zkevm_word_to_field_element<FieldType>(r);
 
-            // 8-bit chunks
-            a8_chunks = zkevm_word_to_field_element_flexible<FieldType>(a, chunk_8_amount, 8);
-            b8_chunks = zkevm_word_to_field_element_flexible<FieldType>(b, chunk_8_amount, 8);
-            for (std::size_t i = 0; i < chunk_8_amount; i++) {
-                allocate(a8_chunks[i], i, 4);
-                allocate(b8_chunks[i], i, 5);
-                a8_chunks_check[i] = a8_chunks[i] * 256;
-                b8_chunks_check[i] = b8_chunks[i] * 256;
-                allocate(a8_chunks_check[i], i, 2);
-                allocate(b8_chunks_check[i], i, 3);
-            }
-        }
+        //     // 8-bit chunks
+        //     a8_chunks = zkevm_word_to_field_element_flexible<FieldType>(a, chunk_8_amount, 8);
+        //     b8_chunks = zkevm_word_to_field_element_flexible<FieldType>(b, chunk_8_amount, 8);
+        //     r8_chunks = zkevm_word_to_field_element_flexible<FieldType>(r, chunk_8_amount, 8);
 
+        //     for (std::size_t i = 0; i < chunk_amount; i++) {
+        //         BOOST_ASSERT(r_chunks[i] == r8_chunks[2*i] + 256 * r8_chunks[2*i + 1]);
+        //     }
+        // }
+
+        // 16-bit chunks allocation
+        // for (std::size_t i = 0; i < chunk_amount; i++) {
+        //     allocate(a_chunks[i], 2 * chunk_amount + i, 6);
+        //     allocate(b_chunks[i], 2 * chunk_amount + i, 7);
+        // }
+
+        // 8-bit chunks allocation
+        // for (std::size_t i = 0; i < chunk_8_amount; i++) {
+        //     // allocate(a8_chunks[i], i, 4);
+        //     // allocate(b8_chunks[i], i, 5);
+        //     // a8_chunks_check[i] = a8_chunks[i] * 256;
+        //     // b8_chunks_check[i] = b8_chunks[i] * 256;
+        //     // allocate(a8_chunks_check[i], i, 2);
+        //     // allocate(b8_chunks_check[i], i, 3);
+        // }
 
         // PART 2: enforcing the multiplication a*b = r (mod 2^256)
-        for (std::size_t i = 0; i < chunk_8_amount; i++) {
-            r8_carryless_chunks[i] = carryless_mul(a8_chunks, b8_chunks, i);
-            TYPE prev_carry = (i > 0) ? r8_carries[i-1] : 0;
-            if constexpr (stage == GenerationStage::ASSIGNMENT) {
-                auto mask8 = (1 << 8) - 1;
-                r8_chunks[i] = (r8_carryless_chunks[i] + prev_carry).to_integral() & mask8;
-                r8_carries[i] = (r8_carryless_chunks[i] + prev_carry).to_integral() >> 8;
-                // std::cout << r8_carryless_chunks[i] <<std::endl;
-                BOOST_ASSERT(r8_carryless_chunks[i] + prev_carry == r8_chunks[i] + 256 * r8_carries[i]);
-            }
-            allocate(r8_chunks[i], i, 6);
+        // for (std::size_t i = 0; i < chunk_8_amount; i++) {
+        //     r8_carryless_chunks[i] = carryless_mul(a8_chunks, b8_chunks, i);
+        //     TYPE prev_carry = (i > 0) ? r8_carries[i-1] : 0;
+        //     if constexpr (stage == GenerationStage::ASSIGNMENT) {
+        //         auto mask8 = (1 << 8) - 1;
+        //         r8_chunks[i] = (r8_carryless_chunks[i] + prev_carry).to_integral() & mask8;
+        //         r8_carries[i] = (r8_carryless_chunks[i] + prev_carry).to_integral() >> 8;
+        //         // std::cout << r8_carryless_chunks[i] <<std::endl;
+        //         BOOST_ASSERT(r8_carryless_chunks[i] + prev_carry == r8_chunks[i] + 256 * r8_carries[i]);
+        //     }
+        //     allocate(r8_chunks[i], i, 6);
 
-            r8_chunks_check[i] = r8_chunks[i] * 256; 
-            allocate(r8_chunks_check[i], i, 7);
-        }
+        //     r8_chunks_check[i] = r8_chunks[i] * 256; 
+        //     allocate(r8_chunks_check[i], i, 7);
+        // }
 
-        for (std::size_t i = 0; i < chunk_8_amount; i++) {
-            // The carries are stored in two columns
-            int column_offset = i % chunk_amount;
-            // int row_offset = i / chunk_amount;
-            int row_offset = (i < 16) ? 0 : 1;
-            allocate(r8_carries[i], 2 * chunk_amount + column_offset, 4 + row_offset);
+        // for (std::size_t i = 0; i < chunk_8_amount; i++) {
+        //     // The carries are stored in two columns
+        //     int column_offset = i % chunk_amount;
+        //     // int row_offset = i / chunk_amount;
+        //     int row_offset = (i < 16) ? 0 : 1;
+        //     allocate(r8_carries[i], 2 * chunk_amount + column_offset, 4 + row_offset);
 
-            // Copy the carries to range-checked columns
-            r8_carries_copy1[i] = r8_carries[i];
-            allocate(r8_carries_copy1[i], 2 * chunk_amount + column_offset, 2 + row_offset);
-            r8_carries_copy2[i] = r8_carries_copy1[i];
-            allocate(r8_carries_copy2[i], i, 1);     // This copy fits in a single row
-        }
+        //     // Copy the carries to range-checked columns
+        //     r8_carries_copy1[i] = r8_carries[i];
+        //     allocate(r8_carries_copy1[i], 2 * chunk_amount + column_offset, 2 + row_offset);
+        //     r8_carries_copy2[i] = r8_carries_copy1[i];
+        //     allocate(r8_carries_copy2[i], i, 1);     // This copy fits in a single row
+        // }
 
-        // TODO: these range checks should fail for large a, b.
-        for (std::size_t i = 0; i < chunk_8_amount; i++) {
-            r8_carries_check[i] = r8_carries_copy2[i] * 256;
-            allocate(r8_carries_check[i], i, 0);
-        }
+        // // TODO: these range checks fail for large a, b. Adjust them appropriately.
+        // for (std::size_t i = 0; i < chunk_8_amount; i++) {
+        //     // r8_carries_check[i] = r8_carries_copy2[i] * 256;
+        //     allocate(r8_carries_check[i], i, 0);
+        // }
     
-        // Ensure consistency between r_chunks and r8_chunks
-        for (std::size_t i = 0; i < chunk_8_amount; i++) {
-            constrain(r_chunks[i] - r8_chunks[2*i] - r8_chunks[2*i + 1] * 256);
-            // TODO: link r_chunks with the actual result
-        }
+        // // Ensure consistency between r_chunks and r8_chunks
+        // // TODO: same for a, b
+        // for (std::size_t i = 0; i < chunk_amount; i++) {
+        //     constrain(r_chunks[i] - r8_chunks[2*i] - r8_chunks[2*i + 1] * 256);  // TODO: this constraint is not satisfied. Why? It should be!
+        //     // TODO: link r_chunks with the actual result
+        // }
 
 
         // PART 3: consistency with the stack
         // TODO. update this part
-        auto A_128 = chunks16_to_chunks128<TYPE>(A);
-        auto B_128 = chunks16_to_chunks128<TYPE>(B);
-        auto R_128 = chunks16_to_chunks128<TYPE>(R);
-        if constexpr( stage == GenerationStage::CONSTRAINTS ){
-            constrain(current_state.pc_next() - current_state.pc(1) - 1);                   // PC transition
-            constrain(current_state.gas(1) - current_state.gas_next() - 5);                 // GAS transition
-            constrain(current_state.stack_size(1) - current_state.stack_size_next() - 1);   // stack_size transition
-            constrain(current_state.memory_size(1) - current_state.memory_size_next());     // memory_size transition
-            constrain(current_state.rw_counter_next() - current_state.rw_counter(1) - 3);   // rw_counter transition
-            std::vector<TYPE> tmp;
-            tmp = rw_table<FieldType, stage>::stack_lookup(
-                current_state.call_id(0),
-                current_state.stack_size(0) - 1,
-                current_state.rw_counter(0),
-                TYPE(0),// is_write
-                A_128.first,
-                A_128.second
-            );
-            lookup(tmp, "zkevm_rw");
-            tmp = rw_table<FieldType, stage>::stack_lookup(
-                current_state.call_id(0),
-                current_state.stack_size(0) - 2,
-                current_state.rw_counter(0) + 1,
-                TYPE(0),// is_write
-                B_128.first,
-                B_128.second
-            );
-            lookup(tmp, "zkevm_rw");
-            tmp = rw_table<FieldType, stage>::stack_lookup(
-                current_state.call_id(1),
-                current_state.stack_size(1) - 2,
-                current_state.rw_counter(1) + 2,
-                TYPE(1),// is_write
-                R_128.first,
-                R_128.second
-            );
-            lookup(tmp, "zkevm_rw");
-        }
+        // auto A_128 = chunks16_to_chunks128<TYPE>(A);
+        // auto B_128 = chunks16_to_chunks128<TYPE>(B);
+        // auto R_128 = chunks16_to_chunks128<TYPE>(R);
+        // if constexpr( stage == GenerationStage::CONSTRAINTS ){
+        //     constrain(current_state.pc_next() - current_state.pc(1) - 1);                   // PC transition
+        //     constrain(current_state.gas(1) - current_state.gas_next() - 5);                 // GAS transition
+        //     constrain(current_state.stack_size(1) - current_state.stack_size_next() - 1);   // stack_size transition
+        //     constrain(current_state.memory_size(1) - current_state.memory_size_next());     // memory_size transition
+        //     constrain(current_state.rw_counter_next() - current_state.rw_counter(1) - 3);   // rw_counter transition
+        //     std::vector<TYPE> tmp;
+        //     tmp = rw_table<FieldType, stage>::stack_lookup(
+        //         current_state.call_id(0),
+        //         current_state.stack_size(0) - 1,
+        //         current_state.rw_counter(0),
+        //         TYPE(0),// is_write
+        //         A_128.first,
+        //         A_128.second
+        //     );
+        //     lookup(tmp, "zkevm_rw");
+        //     tmp = rw_table<FieldType, stage>::stack_lookup(
+        //         current_state.call_id(0),
+        //         current_state.stack_size(0) - 2,
+        //         current_state.rw_counter(0) + 1,
+        //         TYPE(0),// is_write
+        //         B_128.first,
+        //         B_128.second
+        //     );
+        //     lookup(tmp, "zkevm_rw");
+        //     tmp = rw_table<FieldType, stage>::stack_lookup(
+        //         current_state.call_id(1),
+        //         current_state.stack_size(1) - 2,
+        //         current_state.rw_counter(1) + 2,
+        //         TYPE(1),// is_write
+        //         R_128.first,
+        //         R_128.second
+        //     );
+        //     lookup(tmp, "zkevm_rw");
+        // }
     }
 };
 
